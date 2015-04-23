74e63cdaab2769304b65c487bfff9edd16f84f2a
Andrew Emmott <emmott@seward.eecs.oregonstate.edu>
2015-04-23 06:24:13 -0700

    Synced C and cpp code

---
+++ C/frames.c
@@ -0,0 +1,59 @@
+#include "frames.h"
+
+#define CODEGEN_INITFRAME(t) CODEGEN_INITFRAMESIG(t) {\
+    nrow=choice(mat,choice(column_major,ncol(mat),nrow(mat)),nrow);\
+    ncol=choice(mat,choice(column_major,nrow(mat),ncol(mat)),ncol);\
+    t##frame* frame = talloc(t##frame,1);\
+    frame->column_major = column_major;\
+    frame->data = nulloption(mat,matalloc(t,choice(column_major,ncol,nrow),choice(column_major,nrow,ncol)));\
+    frame->nrow = nrow;\
+    frame->ncol = ncol;\
+    return frame;\
+}
+
+#define CODEGEN_CUTFRAME(t) CODEGEN_CUTFRAMESIG(t) {\
+    d(t*)* cutmajors = vecalloc(t*,length(majors));\
+    for_each_in_vec(newidx,oldidxptr,majors,cutmajors[newidx]=tocut->data[*oldidxptr];)\
+    *dim(cutmajors)=*dim(tocut->data);\
+    nrow(cutmajors)=length(majors);\
+    return frame(t,cutmajors,tocut->column_major);\
+}
+
+#define CODEGEN_SPLITFRAME(t) CODEGEN_SPLITFRAMESIG(t) {\
+    t##frame* oldframe = *tosplit;\
+    t##frame* postcut = _cut_##t##frame_(oldframe,majors);\
+    hash_table* umaj = make_hash_table(nrow(oldframe->data));\
+    for_each_in_vec(i,ref,majors,hash_set(umaj,*ref);)\
+    int oldpos = 0;\
+    forseq(newpos,0,nrow(oldframe->data),{\
+        if (!hash_contains(umaj,newpos)) {\
+            oldframe->data[oldpos]=oldframe->data[newpos];\
+            oldpos++;\
+        }\
+    })\
+    nrow(oldframe->data)=oldpos;\
+    choice(oldframe->column_major,oldframe->ncol=oldpos,oldframe->nrow=oldpos);\
+    return postcut;\
+}
+
+#define CODEGEN_CONVFRAME(t,u,cfunc) CODEGEN_CONVFRAMESIG(t,u) {\
+    t##frame* tf = init_frame(t,uf->nrow,uf->ncol,uf->column_major);\
+    for_each_in_frame(m,n,ta,tf,cfunc(ta,uf->data[m][n]);)\
+    return tf;\
+}\
+
+#define CODEGEN_DEFINEFRAME(t)\
+CODEGEN_INITFRAME(t)\
+CODEGEN_CUTFRAME(t)\
+CODEGEN_SPLITFRAME(t)
+
+CODEGEN_DEFINEFRAME(ntstring)
+CODEGEN_DEFINEFRAME(int)
+CODEGEN_DEFINEFRAME(long)
+CODEGEN_DEFINEFRAME(float)
+CODEGEN_DEFINEFRAME(double)
+
+#define str_to_double_fix(fa,str) str_conv_strict(fa,double,str)
+CODEGEN_CONVFRAME(double,ntstring,str_to_double_fix)
+#undef str_to_float_fix
+
---
+++ Makefile
@@ -0,0 +1,36 @@
+CC=gcc
+CFLAGS=--std=c99 -D_GNU_SOURCE -Wall -Werror -g
+PP=g++
+PFLAGS=-Wall -Werror -g
+
+all: cincl.o IsolationForest.o Tree.o utility.o
+
+cincl.o: C/common.o C/object.o C/strfun.o C/readwrite.o C/argparse.o C/argparse_iforest.o C/frames.o
+	ld -r C/common.o C/object.o C/strfun.o C/readwrite.o C/argparse.o C/argparse_iforest.o C/frames.o -o cincl.o
+
+C/%.o: C/%.c C/%.h
+	$(CC) $(CFLAGS) -c $< -o $@
+
+IsolationForest.o: IsolationForest.cpp classes.hpp
+	$(PP) $(PFLAGS) -c $< -o $@
+
+Tree.o: Tree.cpp classes.hpp
+	$(PP) $(PFLAGS) -c $< -o $@
+
+utility.o: utility.cpp utility.h
+	$(PP) $(PFLAGS) -c $< -o $@
+
+main.o: main.cpp utility.h cincl.hpp classes.hpp
+	$(PP) $(PFLAGS) -c $< -o $@
+
+iforest.exe: cincl.o IsolationForest.o Tree.o utility.o main.o
+	$(PP) $(PFLAGS) -o iforest.exe cincl.o IsolationForest.o Tree.o utility.o main.o
+
+fresh:
+	make clean
+	make all
+	make iforest.exe
+
+clean:
+	rm -rf *.o*
+	rm -rf *.exe
