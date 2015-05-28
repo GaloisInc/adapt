ef6f6046b69b42ed86a39d17c56a6ef4a53f14b0
Tadesse Zemicheal <tadesse.habte@gmail.com>
2015-05-27 20:05:26 -0700

    Comment added 	modified:   IsolationForest.cpp 	modified:   IsolationForest.hpp 	modified:   Tree.cpp 	modified:   Tree.hpp 	modified:   main.cpp 	modified:   main.hpp 	modified:   utility.cpp 	modified:   utility.hpp

--- Tree.hpp
+++ Tree.hpp
@@ -3,15 +3,28 @@
  *
  *  Created on: Mar 24, 2015
  *      Author: Tadeze
+ * Tree: class build using the training data
+
  */
 
 #ifndef TREE_H_
 #define TREE_H_
 #include "utility.hpp"
 #include "cincl.hpp"
-
 class Tree
 {
+/*
+ * @param: leftChild: reference to leftChild Tree
+ * @param: rightChild: reference to right child
+ * @param: parent: parent tree ..not very import
+ * @param: nodeSize: number dataset arrived at the node.
+ * @param: splittingAtt: Splitting attribute for the node
+ * @param: splittingPoint: Splitting point for the node
+ * @param: depth: depth of the tree
+ * @param: isLeaf: checks if the node is Leaf node.
+ */
+
+
 public:
 	Tree *leftChild;
 	Tree *rightChild;
@@ -21,6 +34,7 @@ public:
 	double splittingPoint;
 	int depth;
 	bool isLeaf;
+/* Constructor */
 	Tree()
 	{
 		leftChild = NULL;
@@ -34,11 +48,29 @@ public:
 	}
 	;
 
+
+
+/*
+ * Destructor
+ */
 	virtual ~Tree()
-	{	//delete *leftChild; //check if deleting the child is need.
+	{  delete leftChild;
+	   delete rightChild;
 	}
 	;
+	/*
+	 * iTree method: build the tree from the training dataset with specified parameters
+	 * @param: &dIndex  index of the data to run isolation
+	 * @param: height height of the tree
+	 * @param: maxHeight max height of the tree to grow
+	 * @param: stopHeight if tree stop after maxheight
+	 */
 	void iTree(std::vector<int> const &dIndex, int height, int maxHeight, bool stopheight);
+	/*
+	 * pathLength method
+	 * @param: *inst of double feature vector of
+	 * @return pathLength of the instance
+	 */
 	double pathLength(double *inst);
 
 };
