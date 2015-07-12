e05e030ec352c64f3b0b827e0cf3c99162ce754a
Tadesse Zemicheal <tadesse.habte@gmail.com>
2015-07-11 18:54:08 -0700

    IF mod

--- IsolationForest.cpp
+++ IsolationForest.cpp
@@ -12,26 +12,26 @@ IsolationForest::IsolationForest(const int ntree,  int maxheight,
 {
 
 	this->nsample = nsample;
-	this->ntree = ntree;
-	vector<int> sampleIndex;
-	this->rSample = rSample;
-	//build forest through all trees
+ 	this->ntree = ntree;
+  	vector<int> sampleIndex;
+ 	this->rSample = rSample;
+  //build forest through all trees
 	for (int n = 0; n < ntree; n++)
 	{
 		//if sampling is true
-	//Sample and shuffle the data.
-	sampleIndex.clear();
-	if(rSample && nsample<dt->nrow)
-	sampleI(0, dt->nrow - 1, nsample, sampleIndex); //sample nsample
-	else
-	sampleI(0, dt->nrow-1, dt->nrow, sampleIndex);   //shuffle all index of the data if sampling is false
+		//Sample and shuffle the data.
+		sampleIndex.clear();
+		if(rSample && nsample<dt->nrow)
+			sampleI(0, dt->nrow - 1, nsample, sampleIndex); //sample nsample
+		else
+			sampleI(0, dt->nrow-1, dt->nrow, sampleIndex);   //shuffle all index of the data if sampling is false
  
-//build tree	
-Tree *tree = new Tree(); 
-tree->iTree(sampleIndex, 0, maxheight, stopheight);
-this->trees.push_back(tree);
+	       //build tree	
+		Tree *tree = new Tree(); 
+		tree->iTree(sampleIndex, 0, maxheight, stopheight);
+		this->trees.push_back(tree);
 
-	}
+	 }
 
 }
 /*
@@ -47,7 +47,7 @@ double IsolationForest::instanceScore(double *inst)
 			++it)
 	{
 
-	avgPathLength += (*it)->pathLength(inst);  //depth in a tree
+		avgPathLength += (*it)->pathLength(inst);  //depth in a tree
 
 	}
 	double scores;
