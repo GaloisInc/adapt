439b89d7544b7db4b03c46af31bee030991ffc90
Erin Chapman <erin@galois.com>
2015-09-30 15:05:43 -0700

    Squashed 'segment/' content from commit ad38095
    git-subtree-dir: segment
    git-subtree-split: ad380952b37dcf1afa764c6efe3241a1db97c452

---
+++ README.md
@@ -0,0 +1,5 @@
+## Adapt Segmentation Component
+
+- segmenter/ 
+  - This folder Contains an implementation of a simple time-based segmenter for RDF graphs in Turtle format.
+A quite detailed explanation of the segmenter functionality and installation instructions can be found at segmenter/doc/ and segmenter/README, respectively.
---
+++ segmenter/dot2viz.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+import os
+import argparse
+
+def processing(dir, output):
+    files = [f for f in os.listdir(dir) if os.path.isfile(f) and f.endswith(".dot")]
+    for f in files:
+        print "Processing " + f
+        os.system("dot -T " + output + " -O " + f)
+
+if __name__ == "__main__":
+    # Read file
+    parser = argparse.ArgumentParser(description='Convert all dot files in a given directory to other format')
+    parser.add_argument('--format', '-f', nargs='?', default='pdf', type=str, help='pdf/png/ps')
+    parser.add_argument('--dir', '-d', nargs='?', default='.', type=str, help='directory with dot files')
+
+    args = parser.parse_args()
+    processing(args.dir, args.format)
