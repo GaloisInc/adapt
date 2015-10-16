ee8d5977984a6bb05919bb92f9826af3d20b1cb7
Thomas M. DuBuisson <thomas.dubuisson@gmail.com>
2015-10-16 09:47:57 -0700

    Trint: Add simple stats printing.

--- Trint/src/Trint.hs
+++ Trint/src/Trint.hs
@@ -9,18 +9,25 @@ import qualified Graph as G
 
 import Control.Monad (when)
 import Data.Monoid ((<>))
+import Data.Maybe (catMaybes)
 import Data.List (intersperse)
+import Data.Graph
+import qualified Data.Map as Map
+import           Data.Map (Map)
+import MonadLib
+import MonadLib.Monads
 import qualified Data.Text.Lazy as L
 import qualified Data.Text.Lazy.IO as L
 import System.FilePath ((<.>))
 
 data Config = Config { lintOnly   :: Bool
                      , graph      :: Bool
+                     , largestSub :: Bool
                      , files      :: [FilePath]
                      }
 
 defaultConfig :: Config
-defaultConfig = Config False False []
+defaultConfig = Config False False False []
 
 opts :: OptSpec Config
 opts = OptSpec { progDefaults  = defaultConfig
@@ -33,6 +40,9 @@ opts = OptSpec { progDefaults  = defaultConfig
                   , Option ['g'] ["graph"]
                     "Produce a dot file representing a graph of the conceptual model."
                     $ NoArg $ \s -> Right s { graph = True }
+                  , Option [] ["largestSub"]
+                    "Print the size of the largest subgraph (an int)."
+                    $ NoArg $ \s -> Right s { largestSub = True }
                   ]
                }
 
@@ -55,6 +65,7 @@ doRest c fp res
   | lintOnly c = return ()
   | otherwise =
       do when (graph c) (writeFile (fp <.> "dot") (G.graph res))
+         when (largestSub c) (printLargestSubgraph res)
          -- XXX
          -- let doc = T.unlines $ map ppStmt res
          -- when (not lint) (writeFile (fp <.> "trint") doc)
@@ -64,3 +75,39 @@ printWarnings :: [Warning] -> IO ()
 printWarnings ws =
   do let doc = L.unlines $ intersperse "\n" $ map ppWarning ws
      L.putStrLn doc
+
+printLargestSubgraph :: [Stmt] -> IO ()
+printLargestSubgraph ss =
+  do let g  = mkGraph ss
+         vs = vertices g
+         mn = length (take 1 ss) -- one node suggests the minimum subgraph is one.
+         sz = maximum (mn : map (length . reachable g) vs) -- XXX O(n^2) algorithm!
+     putStrLn $ "Largest subgraph is: " ++ show sz
+     putStrLn $ "\tEntities:         " ++ show (length vs)
+     putStrLn $ "\tPredicates:       " ++ show (length ss - length vs)
+     putStrLn $ "\tTotal statements: " ++ show (length ss)
+
+data NodeInfo = Node Int | Edge (Int,Int)
+
+-- Create a graph as an array of edges with nodes represented by Int.
+mkGraph :: [Stmt] -> Graph
+mkGraph ss =
+  let (edges,(_,maxNodes)) = runState (Map.empty, 0) $ catMaybes <$> mapM mkEdge ss
+  in buildG (0,maxNodes) edges
+
+-- Memoizing edge creation
+mkEdge :: Stmt -> State (Map Text Vertex,Vertex) (Maybe Edge)
+mkEdge (StmtPredicate (Predicate s o _ _)) =
+  do nS <- nodeOf s
+     nO <- nodeOf o
+     return $ Just (nS,nO)
+mkEdge _ = return Nothing
+
+-- Memoizing node numbering
+nodeOf :: Text -> State (Map Text Vertex, Vertex) Vertex
+nodeOf name =
+  do (m,v) <- get
+     case Map.lookup name m of
+      Nothing -> do set (Map.insert name v m, v+1)
+                    return v
+      Just n  -> return n
