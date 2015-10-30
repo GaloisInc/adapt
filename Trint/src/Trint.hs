f81db9a42dd8af5918c179a6129212cff43aea4d
Pat Hickey <pat@galois.com>
2015-10-30 11:07:06 -0700

    trint tool: add more options, debugging, start of test suite

--- Trint/src/Trint.hs
+++ Trint/src/Trint.hs
@@ -6,28 +6,44 @@ import Ingest
 import SimpleGetOpt
 import Types as T
 import qualified Graph as G
+import qualified RDF
 
+import Control.Applicative ((<$>))
 import Control.Monad (when)
+import Control.Exception
 import Data.Monoid ((<>))
 import Data.Maybe (catMaybes)
 import Data.List (intersperse)
 import Data.Graph
 import qualified Data.Map as Map
 import           Data.Map (Map)
-import MonadLib
-import MonadLib.Monads
+import MonadLib        hiding (handle)
+import MonadLib.Monads hiding (handle)
 import qualified Data.Text.Lazy as L
 import qualified Data.Text.Lazy.IO as L
 import System.FilePath ((<.>))
 
 data Config = Config { lintOnly   :: Bool
+                     , quiet      :: Bool
                      , graph      :: Bool
                      , stats      :: Bool
+                     , turtle     :: Bool
+                     , ast        :: Bool
+                     , verbose    :: Bool
                      , files      :: [FilePath]
                      }
 
 defaultConfig :: Config
-defaultConfig = Config False False False []
+defaultConfig = Config
+  { lintOnly = False
+  , quiet    = False
+  , graph    = False
+  , stats    = False
+  , turtle   = False
+  , ast      = False
+  , verbose  = False
+  , files    = []
+  }
 
 opts :: OptSpec Config
 opts = OptSpec { progDefaults  = defaultConfig
@@ -37,12 +53,24 @@ opts = OptSpec { progDefaults  = defaultConfig
                   [ Option ['l'] ["lint"]
                     "Check the given file for syntactic and type issues."
                     $ NoArg $ \s -> Right s { lintOnly = True }
+                  , Option ['q'] ["quiet"]
+                    "Quiet linter warnings"
+                    $ NoArg $ \s -> Right s { quiet = True }
+                  , Option ['v'] ["verbose"]
+                    "Verbose debugging messages"
+                    $ NoArg $ \s -> Right s { verbose = True }
+                  , Option ['a'] ["ast"]
+                    "Produce a pretty-printed internal AST"
+                    $ NoArg $ \s -> Right s { ast = True }
                   , Option ['g'] ["graph"]
                     "Produce a dot file representing a graph of the conceptual model."
                     $ NoArg $ \s -> Right s { graph = True }
                   , Option ['s'] ["stats"]
                     "Print statistics."
                     $ NoArg $ \s -> Right s { stats = True }
+                  , Option ['t'] ["turtle"]
+                    "Produce a turtle RDF description of the graph."
+                    $ NoArg $ \s -> Right s { turtle = True }
                   ]
                }
 
@@ -57,19 +85,36 @@ trint c fp =
      case eres of
       Left e            -> L.putStrLn $ "Error: " <> (L.pack $ show e)
       Right (res,ws)    ->
-          do printWarnings ws
+          do unless (quiet c) $ printWarnings ws
              doRest c fp res
 
 doRest :: Config -> FilePath -> [Stmt] -> IO ()
 doRest c fp res
   | lintOnly c = return ()
-  | otherwise =
-      do when (graph c) (writeFile (fp <.> "dot") (G.graph res))
-         when (stats c) (printStats res)
-         -- XXX
-         -- let doc = T.unlines $ map ppStmt res
-         -- when (not lint) (writeFile (fp <.> "trint") doc)
-         return ()
+  | otherwise = do
+      when (graph  c) $ do
+        let dotfile =  fp <.> "dot"
+        dbg ("Writing dot to " ++ dotfile)
+        output dotfile (G.graph res)
+      when (stats  c) $ do
+        printStats res
+
+      when (turtle c) $ do
+        let ttlfile = fp <.> "ttl"
+        dbg ("Writing turtle RDF to " ++ ttlfile)
+        output ttlfile (RDF.turtle res)
+      when (ast c) $ do
+        let astfile = fp <.> "trint"
+        dbg ("Writing ast to " ++ astfile)
+        output astfile $ unlines $ map show res
+        dbg "done"
+  where
+  dbg s = when (verbose c) (putStrLn s)
+  output f s = handle (onError f) $ writeFile f s
+  onError :: String -> IOException -> IO ()
+  onError f e = do putStrLn ("Error writing " ++ f ++ ":")
+                   print e
+
 
 printWarnings :: [Warning] -> IO ()
 printWarnings ws =
