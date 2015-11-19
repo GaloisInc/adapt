b9124fdbb4b70a7072941806e29c6c3b9bd97c40
Erin Chapman <erin@galois.com>
2015-11-18 17:10:36 -0800

    Squashed 'kb/' changes from 1ddf0b9..38fe8a2
    38fe8a2 Fix the declaration parsing interface
    7bfe443 Add a starting point for describing the ssh scenario
    8b0dcde More pretty-printing support
    7bd6a84 Fix some parser bugs, and pretty-print the results
    f310dc0 Ignoring commas in the lexer simplifies the grammar
    af51829 Mostly complete parser
    52a4fb6 Add more of the grammar, and better parse errors
    ed8b416 Fix an export bug in Adapt.Parser
    48942e7 Initial APT parser
    
    git-subtree-dir: kb
    git-subtree-split: 38fe8a24db3fe8f467cad1e091530f56797f4989

---
+++ parser/src/Adapt/Parser/PP.hs
@@ -0,0 +1,36 @@
+module Adapt.Parser.PP (
+    module Adapt.Parser.PP,
+    module Text.PrettyPrint.HughesPJ
+  ) where
+
+import qualified Data.Text.Lazy as L
+import           Text.PrettyPrint.HughesPJ
+
+
+pretty :: PP a => a -> String
+pretty a = show (pp a)
+
+pp :: PP a => a -> Doc
+pp  = ppPrec 0
+
+commas :: [Doc] -> [Doc]
+commas  = punctuate comma
+
+class PP a where
+  ppPrec :: Int -> a -> Doc
+
+  ppList :: [a] -> Doc
+  ppList as = fsep (commas (map pp as))
+
+instance PP a => PP [a] where
+  ppPrec _ as = ppList as
+
+instance PP Int where
+  ppPrec _ = int
+
+instance PP Char where
+  ppPrec _ = char
+  ppList   = text
+
+instance PP L.Text where
+  ppPrec _ t = text (L.unpack t)
---
+++ parser/src/Adapt/Parser/Position.hs
@@ -0,0 +1,117 @@
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Adapt.Parser.Position where
+
+import           Adapt.Parser.PP
+
+import           Data.Function (on)
+import qualified Data.Foldable as F
+import qualified Data.Monoid as M
+import qualified Data.Traversable as T
+import qualified Data.Text.Lazy as L
+
+
+-- Positions -------------------------------------------------------------------
+
+data Position = Position { posRow, posCol, posOff :: {-# UNPACK #-} !Int
+                         } deriving (Show)
+
+instance Eq Position where
+  (==) = (==) `on` posOff
+  (/=) = (/=) `on` posOff
+
+instance Ord Position where
+  compare = compare `on` posOff
+
+instance PP Position where
+  ppPrec _ Position { .. } = pp posRow <> char ':' <> pp posCol
+
+-- | Advance a position by one character.
+advance :: Char -> Position -> Position
+advance '\t' Position { .. } = Position { posCol = posCol + 8
+                                        , posOff = posOff + 8
+                                        , .. }
+
+advance '\n' Position { .. } = Position { posRow = posRow + 1
+                                        , posCol = 1
+                                        , posOff = posOff + 1 }
+
+advance _    Position { .. } = Position { posCol = posCol + 1
+                                        , posOff = posOff + 1
+                                        , .. }
+
+
+-- Ranges ----------------------------------------------------------------------
+
+data Range = NoLoc
+           | Range !Position !Position FilePath
+             deriving (Eq,Show)
+
+instance M.Monoid Range where
+  mempty = NoLoc
+
+  mappend (Range s1 e1 l) (Range s2 e2 _) = Range (min s1 s2) (max e1 e2) l
+  mappend NoLoc b                         = b
+  mappend a NoLoc                         = a
+
+instance PP Range where
+  ppPrec _ NoLoc               = text "<no location>"
+  ppPrec _ (Range start end _) = parens (pp start <> char '-' <> pp end)
+
+
+-- | Select the lines included in the range given, with some amount of context
+-- lines around it.
+getLines :: L.Text -> Int -> Range -> L.Text
+getLines _     _   NoLoc         = L.empty
+getLines input cxt (Range s e _) = L.unlines
+                                 $ take len
+                                 $ drop start
+                                 $ L.lines input
+  where
+
+  (start,len)
+    | cxt >= posRow s = (0,posRow s)
+    | otherwise       = (posRow s - 1 - cxt,cxt + 1)
+
+
+-- Located Things --------------------------------------------------------------
+
+-- | A value paired with a source location.
+data Located a = Located { locRange :: !Range
+                         , locValue :: a
+                         } deriving (Eq,Show,Functor,F.Foldable,T.Traversable)
+
+-- by default, print with no location information
+instance PP a => PP (Located a) where
+  ppPrec p Located { .. } = ppPrec p locValue
+
+
+class HasRange a where
+  getRange :: a -> Range
+
+instance (HasRange a, HasRange b) => HasRange (a,b) where
+  getRange (a,b) = getRange a `M.mappend` getRange b
+
+instance HasRange a => HasRange [a] where
+  getRange = F.foldMap getRange
+  {-# INLINE getRange #-}
+
+instance HasRange a => HasRange (Maybe a) where
+  getRange = F.foldMap getRange
+  {-# INLINE getRange #-}
+
+instance HasRange (Located a) where
+  getRange = locRange
+  {-# INLINE getRange #-}
+
+instance HasRange Range where
+  getRange = id
+  {-# INLINE getRange #-}
+
+
+-- | Convenience function for creating located things.
+at :: HasRange range => a -> range -> Located a
+at locValue range = Located { locRange = getRange range, .. }
