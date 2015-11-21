8d8521324703872bcd6870f44913babd03b342ff
Alexandre Perez <alexandrecperez@gmail.com>
2015-11-20 17:08:38 -0800

    More interesting symptom in the usage example

--- etc/simulator/sim.py
+++ etc/simulator/sim.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
   graph = sd.SegmentationGraph()
   
   graph.generate(0.10, ranks=(6,8), per_rank=(4,5), seed=0)
-  symptom = 10
+  symptom = 23
   
   if len(sys.argv) > 1:
     graph.print_dot()
