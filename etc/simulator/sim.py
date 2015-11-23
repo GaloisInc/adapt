02df03c0a43f2ef3ec759045e61e99b324b1c818
Rui Maranhao <rui.maranhao@gmail.com>
2015-11-23 15:31:57 +0000

    added color to DX

--- etc/simulator/sim.py
+++ etc/simulator/sim.py
@@ -7,12 +7,21 @@ transitions = {'exfiltration': (True, False, ['staging']),
 
 if __name__ == "__main__":
   graph = sd.SegmentationGraph()
-  
+
   graph.generate(0.10, ranks=(6,8), per_rank=(4,5), seed=0)
   symptom = 23
-  
-  if len(sys.argv) > 1:
-    graph.print_dot()
-  else:
+
+
+
+  if len(sys.argv) == 1:
     dx = sd.SimpleDiagnoser(transitions)
     print "Potential APTs: ", dx.diagnose(graph, symptom)
+  else:
+    if(sys.argv[1] == 'pdf'):
+        graph.print_dot()
+    else:
+        dx = sd.SimpleDiagnoser(transitions)
+        dxs = dx.diagnose(graph, symptom)
+        for i in dxs:
+            #print i
+            graph.print_dot(dxs)
