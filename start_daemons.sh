6dba2ea43b1300b90e5914b2e102366323a1913b
John Hanley <John.Hanley@parc.com>
2016-01-11 22:00:14 +0000

    Add start_daemons.sh, and ignore log files from JVM crashes.

---
+++ start_daemons.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Starts daemons: supervisord, zookeeper, kafka, gremlin
+
+# This script is like start.sh,
+# but for folks who prefer tail -f over tmux.
+# Renaming it on top of start.sh would be fine.
+#
+# Also, if supervisord is already running it avoids re-running.
+
+# apt-get wants root to run the daemon. We prefer to run it ourselves.
+if pgrep -U root supervisord > /dev/null
+then
+    sudo service supervisor stop
+fi
+
+ADAPT=$HOME/adapt
+supercfg=$ADAPT/config/supervisord.conf
+
+(set -x
+ cd $ADAPT || exit 1)
+
+# run supervisord (zookeeper, kafka, gremlin)
+if pgrep supervisord > /dev/null
+then
+    exit 0  # Nothing to do, it's already running.
+fi
+(set -x
+ supervisord -c $supercfg)
+
+echo Started.
+# kafka and zookeeper are frustratingly slow and some of the helper
+# scripts do not fail or retry well.
+sleep 5
