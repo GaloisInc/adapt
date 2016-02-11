dfdcb4c7eaee8b1a4260bc065034e7e15ec46ccd
John Hanley <John.Hanley@parc.com>
2016-02-11 10:19:58 -0800

    Extract class.

--- titan/classify_subgraph.py
+++ titan/classify_subgraph.py
@@ -26,19 +26,14 @@ Classifies activities found in subgraphs of a SPADE trace.
 '''
 
 # from prov.model import Identifier, Literal, Namespace, PROV, ProvBundle, XSD
-# from tornado import gen
-# from tornado.concurrent import Future
-# from tornado.ioloop import IOLoop
-# import gremlinclient
-#
+
 # c.f. http://gremlinrestclient.readthedocs.org/en/latest/
 # and  https://github.com/davebshow/gremlinrestclient
-# sudo -H pip3 install gremlinrestclient gremlinclient
+# sudo -H pip3 install gremlinrestclient
 import gremlinrestclient
 import argparse
-import io
+import classify
 import logging
-import os
 import re
 
 __author__ = 'John.Hanley@parc.com'
@@ -46,82 +41,6 @@ __author__ = 'John.Hanley@parc.com'
 log = logging.getLogger(__name__)
 log.addHandler(logging.StreamHandler())
 log.setLevel(logging.INFO)
-
-
-class ExfilDetector(object):
-
-    def __init__(self, k=1):
-        self.k = k
-        self.recent_events = ['' * k]
-        self.cmd = 'unknown'  # This is always the most recent cmd seen.
-        ip_pat = r'(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
-        self._ip_re = re.compile(
-            r'data:destination host="' + ip_pat + '",'
-            r'\s*audit:subtype="network",\s*data:destination port=')
-        self._file_re = re.compile(
-            r'audit:path="(?P<fspec>[\w/\.-]+)",'
-            r'\s*audit:subtype="file",')
-        self._sensitive_re = re.compile(
-            'Company Confidential'
-            '|Xerox Confidential'
-            '|Intel Proprietary'
-            '|For Official Use Only'
-            '|Buckshot Yankee'
-            '|Byzantine Anchor'
-            '|Xkeyscore',
-            re.IGNORECASE)
-        self._dont_read_re = re.compile(  # Binary files, or unreadable dirs.
-            r'^/(dev|proc|var/run'
-            r'|lib/x86_64-linux-gnu'
-            r'|usr/lib/x86_64-linux-gnu'
-            r'|usr/share/locale|usr/lib/sudo)/'
-            r'|^/etc/localtime')
-
-    def remember(self, cmd):
-        '''Maintain a history of recently seen events.'''
-        assert self.k == 1
-        self.recent_events = [cmd]  # Later we'll retain multiple events.
-
-    def is_exfil(self, cmd):
-        '''Predicate is True for sensitive file exfiltration events.'''
-        # recent_foreign_ip = self.get_foreign_ip(self.recent_events[0])
-        return self.is_sensitive_file(cmd)  # and recent_foreign_ip
-
-    def test_is_sensitive_file(self):
-        '''Exercise the several conditional cases.'''
-        audit_tmpl = 'audit:path="%s", audit:subtype="file",'
-        assert self.is_sensitive_file(audit_tmpl % __file__)
-        for tst in ['/non/existent', '/proc/meminfo',
-                    '/etc/issue.net', '/etc/shadow']:
-            assert not self.is_sensitive_file(audit_tmpl % tst)
-
-    def is_sensitive_file(self, cmd):
-        '''Predicate is True for files with restrictive markings.'''
-        # NB: Analysis filesystem must be quite similar to Monitored Host FS.
-        # File paths relative to cwd may require us to track additional state.
-        if (' auditctl ' in cmd and
-                cmd.startswith('sudo ')):
-            return True
-        event = cmd
-        m = self._file_re.search(event)
-        if m:
-            fspec = m.group('fspec')
-            if not os.path.exists(fspec):
-                return False
-            if self._dont_read_re.search(fspec):  # Avoid binary files.
-                return False
-            try:
-                is_sensitive = False
-                with io.open(fspec) as fin:
-                    for line in fin:  # This won't work well on a binary file.
-                        if self._sensitive_re.search(line):
-                            is_sensitive = True
-                return is_sensitive
-            except OSError:
-                # if e.errno != errno.EACCES: print(e)  # 13
-                return False
-        else:
-            return False
 
 
 def get_nodes(db_client):
@@ -140,7 +59,7 @@ def get_nodes(db_client):
         yield node['properties']
 
 
-def get_classifier():
+def get_re_classifier():
     c = []
     for rex, classification in [
             (r'audit:commandline="cat /tmp/timestamp.txt",',
@@ -151,7 +70,6 @@ def get_classifier():
 
 
 def add_vertex(client, cmd, classification):
-
     resp = client.execute(
         "graph.addVertex(label, p1, 'name', p2)",
         bindings={'p1': 'classification', 'p2': classification})
@@ -159,9 +77,9 @@ def add_vertex(client, cmd, classification):
 
 
 def classify_provn_events(url):
-    c = get_classifier()
+    c = get_re_classifier()
     del(c)
-    detector = ExfilDetector()
+    detector = classify.ExfilDetector()
     detector.test_is_sensitive_file()
     client = gremlinrestclient.GremlinRestClient(url=url)
 
