71a375f6b798f730f9bcf206bb09520e08cc7dfd
Thomas M. DuBuisson <tommd@galois.com>
2016-03-25 18:29:56 -0700

    Add a basic unit test framework.
    Unit tests don't buy us much in terms of code coverage for compilers, but we can
    have lots of them and get somewhere useful.  It will also be nice to add
    targeted regression testing.

---
+++ Trint/test/Main.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import           Control.Monad (when)
+import           Data.Map (fromList)
+import           Data.Monoid
+import qualified Data.Text.Lazy as T
+import           Data.Time()
+import           Test.Tasty
+import           Test.Tasty.HUnit
+
+import CommonDataModel.FromProv
+
+main :: IO ()
+main = defaultMain (testGroup "Adapt-Ingest" adaptIngest)
+
+adaptIngest :: [TestTree]
+adaptIngest = [ingest,trint]
+
+trint :: TestTree
+trint = testGroup "trint" []
+
+ingest :: TestTree
+ingest =
+  testGroup "used"
+     [ testCase "Parse Used" $ assertCDMTranslation 
+          ( "used(thesource,thedest, -, [ tc:time=\"2015-09-28T01:06:56Z\", tc:operation=\"read\"])"
+          , [ NodeSubject (Subject { subjectSource = SourceLinuxAuditTrace
+                                   , subjectUID = (9,10,11,12)
+                                   , subjectType = SubjectEvent EventRead Nothing
+                                   , subjectStartTime = read "2015-09-28 01:06:56 UTC"
+                                   , subjectPID = Nothing , subjectPPID = Nothing
+                                   , subjectUnitID = Nothing , subjectEndTime = Nothing
+                                   , subjectCommandLine = Nothing , subjectImportLibs = Nothing
+                                   , subjectExportLibs = Nothing , subjectProcessInfo = Nothing
+                                   , subjectOtherProperties = fromList []
+                                   , subjectLocation = Nothing , subjectSize = Nothing
+                                   , subjectPpt = Nothing , subjectEnv = Nothing
+                                   , subjectArgs = Nothing })]
+          , [ Edge { edgeSource = (1,2,3,4)
+                   , edgeDestination = (9,10,11,12)
+                   , edgeRelationship = WasInformedBy
+                   }
+            , Edge { edgeSource = (9,10,11,12)
+                   , edgeDestination = (5,6,7,8)
+                   , edgeRelationship = Used
+                   }
+            ]
+          )
+     ]
+
+assertCDMTranslation :: (T.Text, [Node], [Edge]) -> IO ()
+assertCDMTranslation (s,nodes,edges) =
+  do let cdm = translateTextCDMPure (cycle [1..31337])
+                 (T.unlines [ "document"
+                            , "prefix tc <http://spade.csl.sri.com/rdf/audit-tc.rdfs#>"
+                            , s
+                            , "end document"
+                            ])
+     when (cdm /= Right (nodes,edges,[]))
+          (assertFailure $ unlines [ "Expected: " <> show (nodes,edges,([] :: [()]))
+                                   , "But got:  " <> show cdm
+                                   ])
