2f32c8783a8a0d2a2ce658a9a06db096bf07d2ef
Rui Maranhao <rui.maranhao@gmail.com>
2016-03-28 14:38:25 -0700

    fix json generation

--- etc/simulator/simulator_diagnoser/graph/segmentation_graph.py
+++ etc/simulator/simulator_diagnoser/graph/segmentation_graph.py
@@ -31,14 +31,14 @@ class SegmentationGraph:
         out.write("}\n")
 
     def print_json(self, dxs=[], out=sys.stdout):
-        out.write("[\n")
+        out.write("data = [\n")
         for n, edge in enumerate(self.G.edges_iter()):
             out.write("  {\"source\":%d, \"target\": %d, \"value\": 0}" % edge)
             if(n != len(list(self.G.edges_iter())) - 1):
                 out.write(",\n")
             else:
                 out.write("\n")
-        out.write("]\n")
+        out.write("];\n")
 
     def generate(self, p, ranks, per_rank, seed=None):
         generation.random_dag(self, p, ranks, per_rank, seed)
