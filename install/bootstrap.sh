7b5926cfa44c384e447b519a94e9b4fd9f4b26ed
Thomas M. DuBuisson <tommd@galois.com>
2016-04-13 16:26:56 -0700

    Do not re-add PPA source, clobber priors.

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -37,7 +37,7 @@ sudo apt-get install git -y
 
 # Install Java 8
 sudo sh -c 'echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections'
-sudo sh -c 'echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list.d/webupd8team-java.list ; echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list.d/webupd8team-java.list ; apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886'
+sudo sh -c 'echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" > /etc/apt/sources.list.d/webupd8team-java.list ; echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list.d/webupd8team-java.list ; apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886'
 sudo apt-get update -y
 sudo apt-get install oracle-java8-installer -y
 
