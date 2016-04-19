1f58e290fe0227c38ae6ffaae5dd530f938c8dad
Thomas M. DuBuisson <tommd@galois.com>
2016-04-19 15:00:28 -0700

    Use kill all instead of grepping the process list.
    N.B. `pidof` didn't work because supervisord is actually a python process and
    doesn't appear properly in the list.  Killall does not have the same issue as
    pidof.

--- install/kick_supervisor.sh
+++ install/kick_supervisor.sh
@@ -2,4 +2,4 @@
 
 # Sends a signal to supervisord that indicates it should re-read the
 # configuration file and kill or restart daemons as necessary given the delta.
-kill -HUP `(ps -e | grep supervisord | cut -d " " -f 1)`
+killall -HUP supervisord
