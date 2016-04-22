0a83d83ca8e90b112216a4ffc339c7a09cca80ef
Thomas M. DuBuisson <tommd@galois.com>
2016-04-21 18:54:43 -0700

    Move to Ubuntu (from Debian).  Rework Vagrant boostrap.

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -1,133 +1,126 @@
 #!/bin/bash
-set -e
 
-# Constants
+KAFKA_ROOT=/opt
 ADAPT_DIR=$HOME/adapt
-USER_BIN=$HOME/.local/bin
-TEMP=$HOME/tmp
-
-TITAN_SERVER_DIR=$ADAPT_DIR/titan
-KAFKA_DIR=$ADAPT_DIR/kafka
 CONFIG_DIR=$ADAPT_DIR/config
+TITAN_SERVER_DIR=/opt/titan
 
-mkdir -p $TEMP
-mkdir -p $USER_BIN
-ret=`echo $PATH | grep "$USER_BIN" ; true`
-if [ -z $ret ]
-then
-    echo "export PATH=\$PATH:$USER_BIN" >> $HOME/.bashrc
-fi
-
-# Verify we can run as root:
-sudo id -u || exit 1
-
-# If we are using vagrant then don't change to the adapt directory,
-# otherwise we can use this same script to stand up a machine though a git
-# clone to $HOME/adapt, so assume that's the case.
-if [ ! \( $HOME = "/home/vagrant" -a ! \( -e $ADAPT_DIR \) \) ]
-then
-    cd $ADAPT_DIR || exit 1
-fi
-
-# Allow PPA's in Debian
-sudo apt-get install build-essential man wget -y
-sudo apt-get install strace tcpdump -y
-sudo apt-get install libssl-dev zlib1g-dev libcurl3-dev libxslt-dev -y
-sudo apt-get install software-properties-common python-software-properties -y
-sudo apt-get install git -y
-
-# Install Java 8
-sudo sh -c 'echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections'
-sudo sh -c 'echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" > /etc/apt/sources.list.d/webupd8team-java.list ; echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list.d/webupd8team-java.list ; apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886'
-sudo apt-get update -y
-sudo apt-get install oracle-java8-installer -y
-
-if [ -e /etc/rc2.d/S20supervisor ]
-then
-    # Adapt runs supervisor as ordinary user,
-    # unlike a vanilla install which would run as root,
-    # perhaps for the host to support unrelated services.
-    echo fatal: a vanilla supervisor install is already present
-    exit 1
-fi
-
-# Tools: git, stack, pip, tmux, wget
-# Entries you might prefer not to use:
-#   openjdk-8-jre-headless -- not available on trusty (use oracle ppa instead)
-#   python-pip -- not advised if versionitis forced using "sudo easy_install pip"
-which pip             || sudo apt-get -y install python-pip
-sudo apt-get -y install \
-    autotools-dev \
-    curl \
-    git \
-    graphviz \
-    jq \
-    libgmp-dev \
-    python \
-    python3-nose \
-    python3-setuptools \
-    supervisor \
-    tmux \
-    unzip \
-    wget \
-    zlib1g-dev \
-
-which pip3 || (sudo -H easy_install3 pip && test -x /usr/bin/pip && sudo rm -f /usr/local/bin/pip)
-sudo -H pip3 install coverage flake8 graphviz gremlinrestclient aiogremlin
-
-# We do not wish for root to launch a supervisor daemon upon reboot.
-sudo rm -f /etc/rc?.d/S20supervisor
-
-if [ ! \( -e $USER_BIN/stack -o -e $TEMP/stack.tar.gz \) ] ; then
-	wget https://www.stackage.org/stack/linux-x86_64 -O $TEMP/stack.tar.gz
-	tar xzf $TEMP/stack.tar.gz -C $TEMP
-fi
-[ -e $USER_BIN/stack ] || cp $TEMP/stack*/stack $USER_BIN
-
-# Python Libraries
-sudo -H pip install kafka-python # protobuf==2.5.0
-
-# Infrastructure Programs (Titan, Gremlin, Zookeeper, kafka)
-GRZIP=$TEMP/titan.zip
-if [ ! -e $GRZIP ] ; then
-	wget http://s3.thinkaurelius.com/downloads/titan/titan-1.0.0-hadoop1.zip -O $GRZIP
-fi
-if [ ! -e $TITAN_SERVER_DIR ] ; then
-	sha256sum $GRZIP | egrep '^67538e231db5be75821b40dd026bafd0cd7451cdd7e225a2dc31e124471bb8ef' > /dev/null  # -e would bail on mismatch
-	unzip   $GRZIP
-	mv titan-1.0.0-hadoop1 $TITAN_SERVER_DIR
-fi
-if [ -e $CONFIG_DIR/titan ] ; then
-    cp -r $CONFIG_DIR/titan/* $TITAN_SERVER_DIR/
-fi
-
-# Use stack to install ghc
-# NB: Trint no longer builds with 7.10.2; you might need: "stack setup 7.10.3"
-$USER_BIN/stack setup
-
-if [ ! \( -e $KAFKA_DIR -o -e $TEMP/kafka.tgz \) ] ; then
-	wget http://www.us.apache.org/dist/kafka/0.8.2.2/kafka_2.11-0.8.2.2.tgz -O $TEMP/kafka.tgz
-	tar xzf $TEMP/kafka.tgz -C .
-fi
-[ -e $KAFKA_DIR ] || mv kafka_2.11-0.8.2.2 $KAFKA_DIR
-
-# Compile all Adapt code
-export PATH=$PATH:$USER_BIN
-
-function install_ingest() {
-    if [ ! \( -e $USER_BIN/Trint \) ]
-    then
-        cd $ADAPT_DIR/ingest/IngestDaemon ; stack install
+handle_error() {
+    lineno=$1
+    if [ ! $FORCE ]; then
+        echo "Failed to provision tc-in-a-box at line $lineno.  See the error above." >&2
+        exit -1
+    else
+        echo "Ignoring tc-in-a-box failure at line $lineno due to force-provision flag."
     fi
 }
 
-function install_dashboard() {
-    if [ ! \( -e $USER_BIN/dashboard \) ]
-    then
-        cd $ADAPT_DIR/dashboard ; stack install
+get_if_hash_differs() {
+    SOURCE=$1
+    DEST=$2
+    VALID_HASH=$3
+    SUDO=$4
+    CURRENT_HASH=''
+    if [ -e $DEST ] ; then
+        CURRENT_HASH="`sha256sum $DEST`"
+    fi
+    if [ ! -e $DEST ] || { [ -z "`echo $CURRENT_HASH | egrep \"^$VALID_HASH\"`" ]; } ; then
+        $SUDO rm $DEST || handle_error $LINENO
+        $SUDO wget $SOURCE -O $DEST || handle_error $LINENO
+    fi
+    if [ ! -z $VALID_HASH ] ; then
+        sha256sum $DEST | egrep "^$VALID_HASH" > /dev/null || handle_error $LINENO
     fi
 }
 
+install_kafka() {
+    KAFKA_VER=$1
+    SCALA_VER=$2
+    VALID_HASH=$3
+    SOURCE=http://apache.mirrors.pair.com/kafka/${KAFKA_VER}/kafka_${SCALA_VER}-${KAFKA_VER}.tgz 
 
-install_ingest
-install_dashboard
+    CWD=$(pwd)
+    cd $KAFKA_ROOT || handle_error $LINENO
+    get_if_hash_differs "$SOURCE" "$KAFKA_ROOT/kafka_${SCALA_VER}-${KAFKA_VER}.tgz" $VALID_HASH sudo
+    sudo tar xvzf kafka_${SCALA_VER}-${KAFKA_VER}.tgz || handle_error $LINENO
+    sudo ln -fsn /opt/kafka_${SCALA_VER}-${KAFKA_VER} /opt/kafka || handle_error $LINENO
+    cd ${CWD} || handle_error $LINENO
+}
+
+install_titan() {
+    CWD=$(pwd)
+    cd /opt || handle_error $LINENO
+    GRZIP=/opt/titan.zip
+    GOODHASH='67538e231db5be75821b40dd026bafd0cd7451cdd7e225a2dc31e124471bb8ef'
+    SOURCE=http://s3.thinkaurelius.com/downloads/titan/titan-1.0.0-hadoop1.zip
+
+    get_if_hash_differs $SOURCE $GRZIP $GOODHASH sudo || handle_error $LINENO
+    sudo rm -rf $TITAN_SERVER_DIR || handle_error $LINENO
+    rm -rf /opt/titan-1.0.0-hadoop1 || handle_error $LINENO
+    sudo unzip $GRZIP || handle_error $LINENO
+    sudo mv titan-1.0.0-hadoop1 $TITAN_SERVER_DIR || handle_error $LINENO
+    cd $CWD || handle_error $LINENO
+}
+
+install_adapt_dependencies() {
+    USER_BIN=$HOME/.local/bin
+    TEMP=$ADAPT_DIR/tmp
+
+    KAFKAVER='0.9.0.1'
+    SCALAVER='2.11'
+    KAFKA_HASH='db28f4d5a9327711013c26632baed8e905ce2f304df89a345f25a6dfca966c7a'
+
+    mkdir -p $TEMP || handle_error $LINENO
+    mkdir -p $USER_BIN || handle_error $LINENO
+    ret=`echo $PATH | grep "$USER_BIN" ; true`
+    if [ -z $ret ]
+    then
+        echo "export PATH=\$PATH:$USER_BIN" >> $HOME/.bashrc || handle_error $LINENO
+    fi
+
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 \
+                     --recv-keys 575159689BEFB442 || handle_error $LINENO
+    echo 'deb http://download.fpcomplete.com/ubuntu trusty main' | sudo tee \
+                /etc/apt/sources.list.d/fpco.list || handle_error $LINENO
+    sudo add-apt-repository ppa:webupd8team/java || handle_error $LINENO
+    sudo apt-get update || handle_error $LINENO
+    echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 boolean true" \
+                | sudo debconf-set-selections || handle_error $LINENO
+    sudo apt-get install -y stack \
+                            python python3-setuptools \
+                            supervisor unzip wget \
+                            python-pip \
+                            oracle-java8-installer || handle_error $LINENO
+    sudo -H easy_install3 pip || handle_error $LINENO
+    sudo -H pip3 install coverage flake8 \
+                        gremlinrestclient aiogremlin || handle_error $LINENO
+    sudo -H pip install kafka-python || handle_error $LINENO
+
+    sudo rm -f /etc/rc?.d/S20supervisor || handle_error $LINENO
+
+    stack setup || handle_error $LINENO
+
+    install_kafka $KAFKAVER $SCALAVER $KAFKA_HASH || handle_error $LINENO
+    install_titan
+    if [ -e $CONFIG_DIR/titan ] ; then
+        sudo cp -r $CONFIG_DIR/titan/* $TITAN_SERVER_DIR/ || handle_error $LINENO
+    fi
+}
+
+function install_adapt() {
+    # Installs: In, PX, Se, AD, AC,DX
+    export PATH=$PATH:$HOME/.local/bin
+
+    # Install ingest system
+    cd $ADAPT_DIR/ingest/IngestDaemon || handle_error $LINENO
+    stack install || handle_error $LINENO
+    cd $ADAPT_DIR/dashboard || handle_error $LINENO
+    stack install || handle_error $LINENO
+    ln -sf $CONFIG_DIR/supervisord.conf.tcinabox $CONFIG_DIR/supervisord.conf || handle_error $LINENO
+}
+
+mkdir $KAFKA_ROOT
+
+fetch_adapt
+install_adapt_dependencies
+install_adapt
