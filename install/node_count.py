abd82f4ccd7a57ead4a49cc4da56ecff59e531f1
John Hanley <John.Hanley@parc.com>
2016-04-28 14:29:19 -0700

    Add shebang. Also, get_event_loop() always returns same object, so avoid loop.close() in case there will be some subsequent operation on it.

--- install/node_count.py
+++ install/node_count.py
@@ -1,14 +1,16 @@
+#! /usr/bin/env python3
+
 import asyncio
 from aiogremlin import GremlinClient
 
 QUERY="g.V().count()"
 
-loop = asyncio.get_event_loop()
-gc = GremlinClient(loop=loop)
-execute = gc.execute(QUERY)
-result = loop.run_until_complete(execute)
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    gc = GremlinClient(loop=loop)
+    execute = gc.execute(QUERY)
+    result = loop.run_until_complete(execute)
 
-print("result: ", result)
+    print("result: ", result)
 
-loop.run_until_complete(gc.close())
-loop.close()
+    loop.run_until_complete(gc.close())
