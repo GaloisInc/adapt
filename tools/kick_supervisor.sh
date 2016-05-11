4b620bd3b0a972280790b13aa8bf34b713818a39
Thomas M. DuBuisson <tommd@galois.com>
2016-05-11 12:08:38 -0700

    Kill java processes when kicking the super.
    Supervisord doesn't restart some of the java processes so we kill them manually.

--- tools/kick_supervisor.sh
+++ tools/kick_supervisor.sh
@@ -2,4 +2,5 @@
 
 # Sends a signal to supervisord that indicates it should re-read the
 # configuration file and kill or restart daemons as necessary given the delta.
+pkill java
 killall -HUP supervisord
