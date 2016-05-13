2dc7d1f8a91348f5ac8f4442eff22fc759473323
Thomas M. DuBuisson <tommd@galois.com>
2016-05-12 20:14:49 -0700

    Don't delay unnecessarily when re-indexing titan.

--- config/titan/scripts/empty-sample.groovy
+++ config/titan/scripts/empty-sample.groovy
@@ -5,12 +5,14 @@ i = mgmt.getGraphIndex('byIdent')
 if(! i) {
   idKey = mgmt.getPropertyKey('ident')
   idKey = idKey ? idKey : mgmt.makePropertyKey('ident').dataType(String.class).make()
-  mgmt.buildIndex('byIdent', Vertex.class).addKey(idKey).buildCompositeIndex()
+  idx = mgmt.buildIndex('byIdent', Vertex.class).addKey(idKey).buildCompositeIndex()
   mgmt.commit()
   graph.tx().commit()
 
   // Wait for index availability
-  mgmt.awaitGraphIndexStatus(graph, 'byIdent').status(SchemaStatus.REGISTERED).call()
+  if ( idx.getIndexStatus(idKey).equals(SchemaStatus.INSTALLED) ) {
+    mgmt.awaitGraphIndexStatus(graph, 'byIdent').status(SchemaStatus.REGISTERED).call()
+  }
   mgmt = graph.openManagement()
   mgmt.updateIndex(mgmt.getGraphIndex('byIdent'), SchemaAction.REINDEX).get()
   mgmt.commit()
