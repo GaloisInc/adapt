3bf4f8fda6204a3bac516d019739ff108e81f2f8
Thomas M. DuBuisson <tommd@galois.com>
2016-05-12 23:17:51 -0700

    Only wait for 'REGISTERED' titan indexes when needed.
    Titan indexes can be in one of a few states.  Initially they are 'installed' and
    once the keys are available they transition to 'registered' and or 'enabled'
    depending on specifics that seem poorly-documented.  Waiting for something to be
    'registered' which is already 'enabled' costs 60 seconds then a timeout of the
    wait routine.
    
    N.B.  REINDEX already costs 3 minutes (no prior data, seems to be a timer as no
    computation is occuring).  As a result, setting up a new system means that it is
    unusable / will through connection exceptions for the first three minutes.

--- config/titan/scripts/empty-sample.groovy
+++ config/titan/scripts/empty-sample.groovy
@@ -1,19 +1,22 @@
-graph = TitanFactory.open('cassandra:localhost')
+// graph = TitanFactory.open('cassandra:localhost')
+graph = TitanFactory.open('/opt/titan/conf/gremlin-server/titan-cassandra-server.properties')
 graph.tx().rollback()
 mgmt = graph.openManagement()
 i = mgmt.getGraphIndex('byIdent')
 if(! i) {
   idKey = mgmt.getPropertyKey('ident')
   idKey = idKey ? idKey : mgmt.makePropertyKey('ident').dataType(String.class).make()
-  idx = mgmt.buildIndex('byIdent', Vertex.class).addKey(idKey).buildCompositeIndex()
+  mgmt.buildIndex('byIdent', Vertex.class).addKey(idKey).buildCompositeIndex()
   mgmt.commit()
   graph.tx().commit()
 
+  mgmt  = graph.openManagement()
+  idKey = mgmt.getPropertyKey('ident')
+  idx   = mgmt.getGraphIndex('byIdent')
   // Wait for index availability
   if ( idx.getIndexStatus(idKey).equals(SchemaStatus.INSTALLED) ) {
     mgmt.awaitGraphIndexStatus(graph, 'byIdent').status(SchemaStatus.REGISTERED).call()
   }
-  mgmt = graph.openManagement()
   mgmt.updateIndex(mgmt.getGraphIndex('byIdent'), SchemaAction.REINDEX).get()
   mgmt.commit()
   mgmt.awaitGraphIndexStatus(graph, 'byIdent').status(SchemaStatus.ENABLED).call()
