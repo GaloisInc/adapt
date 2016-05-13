10f22c9e99dd8571e8c341b44b1518781c42b4c9
Thomas M. DuBuisson <tommd@galois.com>
2016-05-13 00:26:46 -0700

    A DB query tailored to showing quality of ingest on bad-ls
    By comparing the number of nodes, and optionally the numbero f each type of
    node, to known ground truths (bad-ls.histogram), we can determin if Ingest is
    pushing data to the database successfully.

---
+++ tools/validate_histogram.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+
+import asyncio
+from aiogremlin import GremlinClient
+
+QUERY="g.V().count()"
+bad_ls_expected_result=70960
+# Q1="g.V().has('type','file').count()"
+# Q2="g.V().has('type','netflow').count()"
+# Q3="g.V().has('type','memory').count()"
+# Q4="g.V().has('type','resource').count()"
+# Q5="g.V().has('type','subject').count()"
+# Q6="g.V().has('type','host').count()"
+# Q7="g.V().has('type','agent').count()"
+# QUERIES = [Q1,Q2,Q3,Q4,Q5,Q6,Q7]
+QUERIES = []
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    gc = GremlinClient(loop=loop)
+
+    for q in QUERIES:
+        e = gc.execute(q)
+        r = loop.run_until_complete(e)
+        print(q, "\n\t", r[0].data[0])
+
+    execute = gc.execute(QUERY)
+    result = loop.run_until_complete(execute)
+    print("total: ", result[0].data[0])
+    print("success rate: ", result[0].data[0] / bad_ls_expected_result)
+
+    loop.run_until_complete(gc.close())
