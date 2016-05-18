b4738a3b58d94e8103f880ee00bc92b0216e8250
Thomas M. DuBuisson <tommd@galois.com>
2016-05-18 11:00:20 -0700

    Kill and restart titan in a more appropriate manner.

--- tools/restart_services.sh
+++ tools/restart_services.sh
@@ -2,7 +2,6 @@
 
 # Sends a signal to supervisord that indicates it should re-read the
 # configuration file and kill or restart daemons as necessary given the delta.
-pkill java
-sleep 3
+/opt/titan/bin/titan.sh stop
 /opt/titan/bin/titan.sh start
 killall -HUP supervisord
