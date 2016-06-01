6006563c1c6487c287a7d0b2745d37865cfa9352
Thomas M. DuBuisson <tommd@galois.com>
2016-06-01 13:28:14 -0700

    Run generate schema after starting titan.

--- tools/GenerateSchema.py
+++ tools/GenerateSchema.py
@@ -17,8 +17,10 @@ def get_schema():
 
         # generate schema from Language.md to insert in a single transaction
         lines = ["mgmt = graph.openManagement();"]
+        lines.append("if(!mgmt.constainsRelationType('EDGE_SUBJECT_HASPARENT_SUBJECT out')) {")
         for m in matches:
             lines.append("mgmt." + m[0] + ".make();")
+        lines.append("}")
         # We only want one commit within a single transaction, not per schema item.
         lines.append("mgmt.commit();")
 
