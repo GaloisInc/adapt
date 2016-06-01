7c627f8dd168f442ffc6139492cf9aeb8889c206
Thomas M. DuBuisson <tommd@galois.com>
2016-06-01 13:06:42 -0700

    Make node_count.py match schema

--- tools/node_count.py
+++ tools/node_count.py
@@ -3,14 +3,15 @@
 import asyncio
 from aiogremlin import GremlinClient
 
-QUERY="g.V().count()"
-Q1="g.V().has('type','file').count()"
-Q2="g.V().has('type','netflow').count()"
-Q3="g.V().has('type','memory').count()"
-Q4="g.V().has('type','resource').count()"
-Q5="g.V().has('type','subject').count()"
-Q6="g.V().has('type','host').count()"
-Q7="g.V().has('type','agent').count()"
+QUERYV="g.V().count()"
+QUERYE="g.E().count()"
+Q1="g.V().has(label,'File').count()"
+Q2="g.V().has(label,'Entity-NetFlow').count()"
+Q3="g.V().has(label,'Entity-Memory').count()"
+Q4="g.V().has(label,'Resource').count()"
+Q5="g.V().has(label,'Subject').count()"
+Q6="g.V().has(label,'Host').count()"
+Q7="g.V().has(label,'Agent').count()"
 QUERIES = [Q1,Q2,Q3,Q4,Q5,Q6,Q7]
 
 if __name__ == '__main__':
@@ -22,8 +23,12 @@ if __name__ == '__main__':
         r = loop.run_until_complete(e)
         print(q, "\n\t", r)
 
-    execute = gc.execute(QUERY)
+    execute = gc.execute(QUERYV)
     result = loop.run_until_complete(execute)
-    print("total: ", result)
+    print("total nodes: ", result)
+
+    execute = gc.execute(QUERYE)
+    result = loop.run_until_complete(execute)
+    print("total edges: ", result)
 
     loop.run_until_complete(gc.close())
