b6d17345f8e3add6ef77cfa725e258fb2fb0543a
Erin Chapman <erin@galois.com>
2016-06-02 15:19:59 -0700

    Modify testSchema so it's more useful

--- tools/testSchema.py
+++ tools/testSchema.py
@@ -1,19 +1,27 @@
 #! /usr/bin/env python3
 
 import asyncio
-from aiogremlin import GremlinClient
+from aiogremlin import GremlinClient, GremlinServerError
 
-Q1 = "graph.addVertex('Entity')" # should insert
-Q2 = "graph.addVertex('badLabel')" # should fail to insert
-QUERIES = [Q1, Q2]
+QSuccess = "graph.addVertex('Entity')" # should insert
+QFail = "graph.addVertex('badLabel')" # should fail to insert
 
 if __name__ == '__main__':
 	loop = asyncio.get_event_loop()
 	gc = GremlinClient(loop=loop)
 
-	for q in QUERIES:
-		e = gc.execute(q)
+	try:
+        	e = gc.execute(QSuccess)
+        	r = loop.run_until_complete(e)
+        	print(q, "\n\t", r)
+	except:
+		print("Should be able to insert vertex of type 'Entity', schema creation failed")
+
+	try:
+		e = gc.execute(QFail)
 		r = loop.run_until_complete(e)
-		print(q, "\n\t", r)
+		print("Schema creation failed, successful insertion of vertex 'badLabel'")
+	except GremlinServerError:
+		print("Could not insert vertex of type 'badLabel' as expected")
 
 	loop.run_until_complete(gc.close())
