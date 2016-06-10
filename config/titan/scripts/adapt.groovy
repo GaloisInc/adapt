f3c9af8ab633537016630797f8fe583da17d61f8
Thomas M. DuBuisson <tommd@galois.com>
2016-06-09 20:22:47 -0700

    Refactor adapt.groovy.
    I've factored out the index insertion scripts into functions.  I believe these
    functions are properly parameterized such that they will be useful for many of
    the other scripts needed by the project.
    
    IF YOU WANT TO ADD AN INDEX:
    
    1. Decide if you want equality or richer search
    2. If you want equality then use makeNodeIndex.
    3. If you need search that is richer than equality (c.f. ElasticSearch) then use
    makeElasticSearchIndex.
    4. Provide the index name, which can be arbitrary but must not conflict with
    other names and in the case of elastic search will be needed in the queries.
    5. Provide a single index key.  You can make indexes over multiple keys but the
    provided functions just encode the method for single-key indexes.
    6. Provide the type, such as String.class, which will be indexed.

--- config/titan/scripts/adapt.groovy
+++ config/titan/scripts/adapt.groovy
@@ -1,50 +1,70 @@
 graph = TitanFactory.open('/opt/titan/conf/gremlin-server/titan-cassandra-server.properties')
 graph.tx().rollback()
-mgmt = graph.openManagement()
-i = mgmt.getGraphIndex('byIdent')
-if(! i) {
-  idKey = mgmt.getPropertyKey('ident')
-  idKey = idKey ? idKey : mgmt.makePropertyKey('ident').dataType(String.class).make()
-  mgmt.buildIndex('byIdent', Vertex.class).addKey(idKey).buildCompositeIndex()
-  mgmt.commit()
-  graph.tx().commit()
 
-  mgmt  = graph.openManagement()
-  idKey = mgmt.getPropertyKey('ident')
-  idx   = mgmt.getGraphIndex('byIdent')
-  // Wait for index availability
-  if ( idx.getIndexStatus(idKey).equals(SchemaStatus.INSTALLED) ) {
-    mgmt.commit()
-    mgmt.awaitGraphIndexStatus(graph, 'byIdent').status(SchemaStatus.REGISTERED).call()
-  } else { mgmt.commit() }
-  mgmt  = graph.openManagement()
-  mgmt.updateIndex(mgmt.getGraphIndex('byIdent'),SchemaAction.ENABLE_INDEX).get()
-  mgmt.commit()
-  mgmt.awaitGraphIndexStatus(graph, 'byIdent').status(SchemaStatus.ENABLED).call()
-} else { mgmt.commit() }
+// makeNodeIndex: Makes a node-centric index for fast lookup
+// A Node index is a Titan-only index mechanism that supports equality queries
+// such as `g.has(indexKey, 'somevalue').next()`
+def makeNodeIndex = { String indexName, String indexKey, indexType ->
+    mgmt = graph.openManagement()
+    i = mgmt.getGraphIndex(indexName)
+    if(! i) {
+      idKey = mgmt.getPropertyKey(indexKey)
+      idKey = idKey ? idKey : mgmt.makePropertyKey(indexKey).dataType(indexType).make()
+      mgmt.buildIndex(indexName, Vertex.class).addKey(idKey).buildCompositeIndex()
+      mgmt.commit()
+      graph.tx().commit()
 
-// URL index
-mgmt = graph.openManagement()
-i = mgmt.getGraphIndex('byURL')
-if(! i) {
-  urlKey = mgmt.getPropertyKey('url')
-  urlKey = urlKey ? urlKey : mgmt.makePropertyKey('url').dataType(String.class).make()
-  mgmt.buildIndex('byURL', Vertex.class).addKey(urlKey, Mapping.STRING.asParameter()).buildMixedIndex('search')
-  mgmt.commit()
-  graph.tx().commit()
+      mgmt  = graph.openManagement()
+      idKey = mgmt.getPropertyKey(indexKey)
+      idx   = mgmt.getGraphIndex(indexName)
+      // Wait for index availability
+      if ( idx.getIndexStatus(idKey).equals(SchemaStatus.INSTALLED) ) {
+        mgmt.commit()
+        mgmt.awaitGraphIndexStatus(graph, indexName).status(SchemaStatus.REGISTERED).call()
+      } else { mgmt.commit() }
+      mgmt  = graph.openManagement()
+      mgmt.updateIndex(mgmt.getGraphIndex(indexName),SchemaAction.ENABLE_INDEX).get()
+      mgmt.commit()
+      mgmt.awaitGraphIndexStatus(graph, indexName).status(SchemaStatus.ENABLED).call()
+    } else { mgmt.commit() }
+}
 
-  mgmt  = graph.openManagement()
-  urlKey = mgmt.getPropertyKey('url')
-  idx   = mgmt.getGraphIndex('byURL')
-  // Wait for index availability
-  if ( idx.getIndexStatus(urlKey).equals(SchemaStatus.INSTALLED) ) {
-    mgmt.commit()
-    mgmt.awaitGraphIndexStatus(graph, 'byURL').status(SchemaStatus.REGISTERED).call()
-  } else { mgmt.commit() }
-  mgmt  = graph.openManagement()
-  mgmt.updateIndex(mgmt.getGraphIndex('byURL'),SchemaAction.ENABLE_INDEX).get()
-  mgmt.commit()
-  mgmt.awaitGraphIndexStatus(graph, 'byURL').status(SchemaStatus.ENABLED).call()
-} else { mgmt.commit() }
+// Values indexed via an external tool like elastic search provide for a richer query
+// lanaugage.  For example, the elastic search query of url's with a regular expression
+// is `graph.queryIndex('byURL','v.url:/.*bin.ls.*/').vertices()`
+def makeElasticSearchIndex = { String indexName, String indexKey, indexType ->
+    mgmt = graph.openManagement()
+    i = mgmt.getGraphIndex(indexName)
+    if(! i) {
+      urlKey = mgmt.getPropertyKey(indexKey)
+      urlKey = urlKey ? urlKey : mgmt.makePropertyKey(indexKey).dataType(indexType).make()
+      mgmt.buildIndex(indexName, Vertex.class).addKey(urlKey, Mapping.STRING.asParameter()).buildMixedIndex('search')
+      mgmt.commit()
+      graph.tx().commit()
+    
+      mgmt  = graph.openManagement()
+      urlKey = mgmt.getPropertyKey(indexKey)
+      idx   = mgmt.getGraphIndex(indexName)
+      // Wait for index availability
+      if ( idx.getIndexStatus(urlKey).equals(SchemaStatus.INSTALLED) ) {
+        mgmt.commit()
+        mgmt.awaitGraphIndexStatus(graph, indexName).status(SchemaStatus.REGISTERED).call()
+      } else { mgmt.commit() }
+      mgmt  = graph.openManagement()
+      mgmt.updateIndex(mgmt.getGraphIndex(indexName),SchemaAction.ENABLE_INDEX).get()
+      mgmt.commit()
+      mgmt.awaitGraphIndexStatus(graph, indexName).status(SchemaStatus.ENABLED).call()
+    } else { mgmt.commit() }
+}
 
+// We index the 'ident' field, which matches CDM 'UUID' but as a Base64 string.
+makeNodeIndex('byIdent','ident',String.class)
+
+
+// URL index use ElasticSearch which provides richer queries
+// including regex
+makeElasticSearchIndex('byURL','url',String.class)
+
+// The graph traverser captured by variable 'g' is useful to many of
+// the insertion and query commands used in normal operation.
 g = graph.traversal()
