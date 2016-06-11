f5b5e6fa9719057c983ef9df8c2930d21d1e84e1
John Hanley <John.Hanley@parc.com>
2016-06-11 13:47:01 -0700

    Squashed 'segment/' changes from 5cebb2d..384156d
    384156d Specify that the broker runs on port 8182.
    dbe1807 Add shebang.
    408e1d7 Import os before calling os.system().
    3aa85d8 Stick to SI units, avoiding multiple ways for the API to express the same thing.
    79fe0af Delete fake-segment.py
    4b6a428 Executable files should be marked executable.
    0efa4e7 default spec
    d25a853 * attempt at segmenter daemon
    697d875 Add exception for timestamp in includes event
    f694327 Fix add_node
    7a1c202 Add --store_segment option to store segmentation result into DB
    7c4bbb2 Add union operation on documents
    52896fc Fixes logic of the program: read from db if provn is not provided
    aa1a91e Added first version of adapt segmenter with titandb interface
    4b6f75c UPdated requeriments
    fc1d83d Minor changes
    a5e5c8f Added Titan client with aiogremlin
    c2f9a0e Adapted code to python3
    2556bb9 Merge branch 'master' of https://github.com/GaloisInc/Adapt-segment
    1e710a2 titan wrapper wip
    f85314c Merge branch 'master' of https://github.com/GaloisInc/Adapt-segment
    4b8c5f0 gitignore
    4a3b011 Added test function. Run it with: nosetests provn_segmenter.py
    af54308 * oops
    95f9b57 * quick and dirty attempt to formalize the semantics more carefully
    28df781 Update README.md
    a232261 * additional tests
    1b1e803 Merge branch 'master' of github.com:GaloisInc/Adapt-segment
    464ac9d Update README.md
    236330e Fixed bug involving repeated segments in output. Fixed datetime addition.
    01f6dda Fixed output format
    9073010 * readme, error in test
    e6786ee * usage
    7dbe241 * oops
    d65e936 * segmenter phase 1
    f25ef70 * README.md
    d54be41 * README.md
    d051ade * add README.md
    acb1096 * updated README
    dc1142b updated requirements and removed prints
    f73a88d removed drawing function and corresponding dependency
    864ba21 First version of the provn specification-based segmenter
    00bd41e Merge branch 'master' of https://github.com/GaloisInc/Adapt-segment
    a408394 First version of the provn specification-based segmenter
    63b0f11 * segmenter spec example
    0d4d650 initial import: provn parser and some test provn files
    0221bd4 Extended segmenter to segment a graph for all possibled values of a given key (e.g. pid)
    359fe5a Initial import: a distance-based segmenter.
    
    git-subtree-dir: segment
    git-subtree-split: 384156dd3291bdbe43a4de680bd854d73c0ba74a

---
+++ segmenter/provnparser.py
@@ -0,0 +1,420 @@
+"""
+    =======
+    `Parser`
+    =======
+
+    This module contains a pyparsing bnf for
+    provn.
+
+    Adria Gascon, 2016.
+"""
+
+from pyparsing import *
+import sys
+
+
+class ProvRelation:
+    def __init__(self, s, t, att_val_list, timestamp=None):
+        self.s = s
+        self.t = t
+        self.timestamp = None if timestamp == '-' else timestamp
+        self.att_val_dict = dict([(k, v) for (k, v) in att_val_list])
+        self.name = None
+
+
+class SegmentExpr(ProvRelation):
+    def __str__(self):
+        return 'includes({0}, {1}, [{2}])'.format(
+            self.s, self.t, ','.join(['{0}=\"{1}\"'.format(k, v)
+                for k, v in self.att_val_dict.items()]))
+
+    def label(self):
+        return 'includes'
+
+
+class CommExpr(ProvRelation):
+    def __str__(self):
+        return 'wasInformedBy({0}, {1}, {2})'.format(
+            self.s, self.t, self.att_val_dict)
+
+    def label(self):
+        return 'wasInformedBy'
+
+
+class DerivationExpr(ProvRelation):
+    def __str__(self):
+        return 'wasDerived({0}, {1}, {2})'.format(
+            self.s, self.t, self.att_val_dict)
+
+    def label(self):
+        return 'wasDerivedFrom'
+
+
+class AssociationExpr(ProvRelation):
+    def __str__(self):
+        return 'wasAssociatedWith({0}, {1}, {2}, {3})'.format(
+            self.s, self.t,
+            self.timestamp if self.timestamp else '-'   , self.att_val_dict)
+
+    def label(self):
+        return 'wasAssociatedWith'
+
+
+class UsageExpr(ProvRelation):
+    def __str__(self):
+        return 'used({0}, {1}, {2}, {3})'.format(
+            self.s, self.t, self.timestamp, self.att_val_dict)
+
+    def label(self):
+        return 'used'
+
+
+class ValidationExpr(ProvRelation):
+    def __str__(self):
+        return 'wasInvalidatedBy({0}, {1}, {2}, {3})'.format(
+            self.s, self.t,
+            self.timestamp, self.att_val_dict)
+
+    def label(self):
+        return 'wasInvalidatedBy'
+
+
+class GenerationExpr(ProvRelation):
+    def __str__(self):
+        return 'wasGeneratedBy({0}, {1}, {2}, {3})'.format(
+            self.s, self.t, self.timestamp, self.att_val_dict)
+
+    def label(self):
+        return 'wasGeneratedBy'
+
+
+class Segment:
+    def __init__(self, id_, att_val_list):
+        self.id = id_
+        self.att_val_dict = dict([(k, v) for (k, v) in att_val_list])
+
+    def __str__(self):
+        return 'segment({0},[{1}])'.format(self.id,
+            ','.join(['{0}=\"{1}\"'.format(k, v)
+                for k, v in self.att_val_dict.items()]))
+
+    def label(self):
+        return 'segment'
+
+
+class Agent:
+    def __init__(self, id_, att_val_list):
+        self.id = id_
+        self.att_val_dict = dict([(k, v) for (k, v) in att_val_list])
+
+    def __str__(self):
+        return 'agent({0},{1})'.format(self.id, self.att_val_dict)
+
+    def label(self):
+        return 'agent'
+
+
+class Activity:
+    def __init__(self, id_, att_val_list):
+        self.id = id_
+        self.att_val_dict = dict([(k, v) for (k, v) in att_val_list])
+
+    def __str__(self):
+        return 'activity({0},{1})'.format(self.id, self.att_val_dict)
+
+    def label(self):
+        return 'activity'
+
+
+class Entity:
+    def __init__(self, id_, att_val_list):
+        self.id = id_
+        self.att_val_dict = dict([(k, v) for (k, v) in att_val_list])
+
+    def __str__(self):
+        return 'entity({0},{1})'.format(self.id, self.att_val_dict)
+
+    def label(self):
+        return 'entity'
+
+
+class ResourceFactory:
+    @classmethod
+    def create(cls, type_, id_, att_val_list=[]):
+        if type_ == 'activity':
+            return Activity(id_, att_val_list)
+        elif type_ == 'entity':
+            return Entity(id_, att_val_list)
+        elif type_ == 'agent':
+            return Agent(id_, att_val_list)
+        elif type_ == 'segment':
+            return Segment(id_, att_val_list)
+        raise Exception('Unknown resource type: {}'.format(type_))
+
+
+class EventFactory:
+    @classmethod
+    def create(cls, type_, s, t, att_val_list, timestamp=None):
+        if type_ == 'includes':
+            return SegmentExpr(s, t, att_val_list, timestamp)
+        elif type_ == 'wasInformedBy':
+            return CommExpr(s, t, att_val_list, timestamp)
+        elif type_ == 'wasDerivedFrom':
+            return DerivationExpr(s, t, att_val_list, timestamp)
+        elif type_ == 'wasAssociatedWith':
+            return AssociationExpr(s, t, att_val_list, timestamp)
+        elif type_ == 'used':
+            return UsageExpr(s, t, att_val_list, timestamp)
+        elif type_ == 'wasInvalidatedBy':
+            return ValidationExpr(s, t, att_val_list, timestamp)
+        elif type_ == 'wasGeneratedBy':
+            return GenerationExpr(s, t, att_val_list, timestamp)
+        raise Exception('Unknown event type: {}'.format(type_))
+
+
+class PrefixDecl:
+    def __init__(self, id_, url):
+        self.id = id_
+        self.url = url
+
+    def __str__(self):
+        return 'prefix {0} {1}'.format(self.id, self.url)
+
+
+class Document:
+    def __init__(self):
+        self.filename = None
+        self.expression_list = []
+        self.prefix_decls = []
+
+    def parse_provn(self, filename):
+        self.filename = filename
+        bnf(self)['document'].parseFile(filename, True)
+        assert self.expression_list != None
+        assert self.prefix_decls != None
+
+    def union(self, doc):
+        for e in doc.expression_list:
+            self.expression_list.append(e)
+        self.prefix_decls = list(set(self.prefix_decls) | set(doc.prefix_decls))
+
+    def __str__(self):
+        def f(x): return '\t' + str(x)
+        l = ['document']
+        l += map(f, self.prefix_decls)
+        l += map(f, self.expression_list)
+        l += ['endDocument']
+        return '\n'.join(l)
+
+    ########################################################
+    # Parsing actions
+
+    def make_document(self, t):
+        """
+        Parsing action for the Program non-terminal
+        """
+        self.prefix_decls = t['prefix_decls']
+        self.expression_list = t['expression_list']
+
+    def make_expression(self, t):
+        # This is to make sure that we do not miss any expression
+        assert isinstance(t[0], Activity) or \
+               isinstance(t[0], CommExpr) or \
+               isinstance(t[0], UsageExpr) or \
+               isinstance(t[0], DerivationExpr) or \
+               isinstance(t[0], GenerationExpr) or \
+               isinstance(t[0], Agent) or \
+               isinstance(t[0], ValidationExpr) or \
+               isinstance(t[0], AssociationExpr) or \
+               isinstance(t[0], Entity), t
+
+    def make_prefix_decl(self, t):
+        return PrefixDecl(t['id'], t['url'])
+
+    def make_activity_expression(self, t):
+        return Activity(t['id'][0], t['att_val_list'])
+
+    def make_agent_expression(self, t):
+        return Agent(t['id'][0], t['att_val_list'])
+
+    def make_entity_expression(self, t):
+        return Entity(t['id'][0], t['att_val_list'])
+
+    def make_usage_expression(self, t):
+        try:
+            return UsageExpr(t['s'][0], t['t'][0],
+                t['att_val_list'], t['timestamp'])
+        except KeyError:
+            return UsageExpr(t['s'][0], t['t'][0], [], t['timestamp'])
+
+    def make_association_expression(self, t):
+        try:
+            return AssociationExpr(
+                t['s'][0], t['t'][0], t['att_val_list'], t['timestamp'])
+        except KeyError:
+            return AssociationExpr(
+                t['s'][0], t['t'][0], [], t['timestamp'])
+
+    def make_validation_expression(self, t):
+        try:
+            return ValidationExpr(
+                t['s'][0], t['t'][0], t['att_val_list'], t['timestamp'])
+        except KeyError:
+            return ValidationExpr(t['s'][0], t['t'][0], [], t['timestamp'])
+
+    def make_generation_expression(self, t):
+        try:
+            return GenerationExpr(
+                t['s'][0], t['t'][0], t['att_val_list'], t['timestamp'])
+        except KeyError:
+            return GenerationExpr(
+                t['s'][0], t['t'][0], [], t['timestamp'])
+
+    def make_communication_expression(self, t):
+        try:
+            return CommExpr(
+                t['s'][0], t['t'][0], t['att_val_list'], t['timestamp'])
+        except KeyError:
+            timestamp = t.get('timestamp', None)
+            return CommExpr(t['s'][0], t['t'][0], [], timestamp)
+
+    def make_derivation_expression(self, t):
+        timestamp = t.get('timestamp', None)
+        return DerivationExpr(
+            t['s'][0], t['t'][0], t['att_val_list'], timestamp)
+
+    def make_att_val_pair(self, t):
+        return (t['att'], t['val'])
+
+
+def bnf(doc):
+    """
+    Defines the Grammar for provn
+    :return: Pyparsing objects for parsing provn
+    """
+    """
+    Grammar Definition
+    """
+    lbrack = Suppress("[")
+    rbrack = Suppress("]")
+    lpar = Literal("(")
+    rpar = Literal(")")
+    colon = Literal(":")
+    comma = Suppress(",")
+    double_quote = Suppress('\"')
+    equal = Suppress("=")
+    prefix_name = Word(alphanums + '-')
+    name = Word(alphanums)
+    word_with_spaces = Word(alphanums + ' /:_-().,{}[]+*=$')
+    dash = '-'
+    la = '<'
+    ra = '>'
+    url_word = Word(alphanums + '_/-#:.')
+    date = Word(nums + '-')
+    time = Word(nums + ':')
+    timestamp = Combine(date + 'T' + time)
+    # Identifiers and att-value pairs
+    identifier = Combine(prefix_name.setResultsName('prefix') + colon +
+        name.setResultsName('name'))
+    att = Combine(prefix_name + colon + name).setResultsName('att')
+    att_val_pair = Combine(att + equal +
+        double_quote + word_with_spaces.setResultsName('val') + double_quote).\
+        setParseAction(doc.make_att_val_pair)
+
+    # Expressions
+    generation_expr = (Keyword('wasGeneratedBy') + lpar +
+        identifier.setResultsName('s') + comma +
+        identifier.setResultsName('t') +
+        Optional(comma + (timestamp | dash).setResultsName('timestamp')) +
+        Optional(
+            comma +
+            (lbrack + delimitedList(att_val_pair)
+                .setResultsName('att_val_list') +
+            rbrack) | (lbrack + rbrack)) +
+        rpar).setParseAction(doc.make_generation_expression)
+    activity_expr = (Keyword('activity') + lpar +
+        identifier.setResultsName('id') + comma + lbrack +
+        delimitedList(att_val_pair).setResultsName('att_val_list') + rbrack +
+            rpar).setParseAction(doc.make_activity_expression)
+    agent_expr = (Keyword('agent') + lpar +
+        identifier.setResultsName('id') + comma + lbrack +
+        delimitedList(att_val_pair).setResultsName('att_val_list') + rbrack +
+            rpar).setParseAction(doc.make_agent_expression)
+    entity_expr = (Keyword('entity') + lpar +
+        identifier.setResultsName('id') + comma + lbrack +
+        delimitedList(att_val_pair).setResultsName('att_val_list') + rbrack +
+            rpar).setParseAction(doc.make_entity_expression)
+    usage_expr = (Keyword('used') + lpar +
+        identifier.setResultsName('s') + comma +
+        identifier.setResultsName('t') +
+        Optional(comma + (timestamp | dash).setResultsName('timestamp')) +
+        Optional(
+            comma +
+            (lbrack + delimitedList(att_val_pair)
+                .setResultsName('att_val_list') +
+            rbrack) | (lbrack + rbrack)) +
+        rpar).setParseAction(doc.make_usage_expression)
+    association_expr = (Keyword('wasAssociatedWith') + lpar +
+        identifier.setResultsName('s') + comma +
+        identifier.setResultsName('t') +
+        Optional(comma + (timestamp | dash).setResultsName('timestamp')) +
+        Optional(
+            comma +
+            (lbrack + delimitedList(att_val_pair)
+                .setResultsName('att_val_list') +
+            rbrack) | (lbrack + rbrack)) +
+        rpar).setParseAction(doc.make_association_expression)
+    validation_expr = (Keyword('wasInvalidatedBy') + lpar +
+        identifier.setResultsName('s') + comma +
+        identifier.setResultsName('t') +
+        Optional(comma + (timestamp | dash).setResultsName('timestamp')) +
+        Optional(
+            comma +
+            (lbrack + delimitedList(att_val_pair)
+                .setResultsName('att_val_list') +
+            rbrack) | (lbrack + rbrack)) +
+        rpar).setParseAction(doc.make_validation_expression)
+    communication_expr = (Keyword('wasInformedBy') + lpar +
+        identifier.setResultsName('s') + comma +
+        identifier.setResultsName('t') + comma +
+        ((lbrack + delimitedList(att_val_pair).setResultsName('att_val_list') +
+        rbrack) | (lbrack + rbrack)) + rpar).\
+        setParseAction(doc.make_communication_expression)
+    derivation_expr = (Keyword('wasDerivedFrom') + lpar +
+        identifier.setResultsName('s') + comma +
+        identifier.setResultsName('t') + comma +
+        ((lbrack + delimitedList(att_val_pair).setResultsName('att_val_list') +
+        rbrack) | (lbrack + rbrack)) + rpar).\
+        setParseAction(doc.make_derivation_expression)
+    expression = ( association_expr | activity_expr | communication_expr | entity_expr | usage_expr | generation_expr | derivation_expr | agent_expr | validation_expr).\
+        setParseAction(doc.make_expression)
+    prefix_decl = (Keyword('prefix') + prefix_name.setResultsName('id') + la +
+        url_word.setResultsName('url') + ra).\
+        setParseAction(doc.make_prefix_decl)
+
+    # Document
+    document = (Keyword('document') +
+        OneOrMore(prefix_decl).setResultsName('prefix_decls') +
+        OneOrMore(expression).setResultsName('expression_list') +
+        Keyword('endDocument'))
+    document.setParseAction(doc.make_document).setParseAction(
+        doc.make_document)
+
+    return {"document": document}
+
+
+if __name__ == "__main__":
+    def test(s, nt):
+        doc = Document()
+        result = bnf(doc)[nt].parseString(s, True)
+        print(s, '\n\t=>\n', result)
+        print('---------------------')
+
+    def test_file(f, nt):
+        doc = Document()
+        result = bnf(doc)[nt].parseFile(f, True)
+        print(f, '\n\t=>\n', result)
+        print('---------------------')
+
+    f = sys.argv[1]
+    test_file(f, 'document')
---
+++ segmenter/titandb.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    =======
+    `TitanClient`
+    =======
+
+    Adria Gascon, 2016.
+"""
+import asyncio
+from aiogremlin import GremlinClient
+from provn_segmenter import DocumentGraph, Document
+from provnparser import ResourceFactory, EventFactory
+import re
+import argparse
+import os
+import sys
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class TitanClient:
+    def __init__(self, broker):
+        self.broker = broker
+        assert not self.broker or re.match('.+:\d+', self.broker),\
+            'Broker must be in format url:port'
+        self.loop = asyncio.get_event_loop()
+        self.gc = GremlinClient(url=broker, loop=self.loop)
+
+    def close(self):
+        self.loop.run_until_complete(self.gc.close())
+        self.loop.close()
+
+    def execute(self, gremlin_query_str, bindings={}):
+        execute = self.gc.execute(gremlin_query_str, bindings=bindings)
+        logger.debug('QUERY:\n {}'.format(gremlin_query_str))
+        try:
+            result = self.loop.run_until_complete(execute)
+        except Exception as e:
+            print('Error trying to connect to Titan DB: {0}...aborting'.
+                format(e))
+            self.close()
+            sys.exit(-1)
+        assert result[0].status_code in (200, 204, 206), result[0].status_code
+        if result == 204:
+            return None
+        else:
+            return result[0].data
+
+    def all_edges(self):
+        return self.execute('g.E()')
+
+    def all_nodes(self):
+        return self.execute('g.V()')
+
+    def add_edge(self, n1, d1, n2, d2, d, label):
+        """
+        Adds edge with label and dict d between nodes with names n1 and n2,
+        if it does not exist.
+        If the nodes do not exist they are created with dicts d1 and d2.
+        """
+        r1 = self.add_node(n1, d1)
+        r2 = self.add_node(n2, d2)
+        id1 = r1[0]['id']
+        id2 = r2[0]['id']
+        properties_str = ', '.join(
+            map(lambda x: '\'{0}\',\'{1}\''.format(x[0], x[1]), d.items()))
+        r = self.execute(
+            'g.V({0}).next().addEdge(\'{2}\', g.V({1}).next(), {3})'.format(
+                id1, id2, label, properties_str))
+        return r
+
+    def add_node(self, n, d):
+        """
+        Adds node with name n to the DB if it does not already exist.
+        A node with name n and dictionary d exists in the DB if
+          c1) there is already a node with name n in the db, or
+          c2) there is already a node with dictionary d in the db
+        """
+        d['name'] = n
+        c1 = self.execute('g.V().has(\'name\', \'{}\')'.format(n))
+        properties_str = ', '.join(
+            map(lambda x: '__().has(\'{0}\',\'{1}\')'.format(
+                x[0], x[1]), d.items()))
+        c2 = self.execute('g.V().and({0})'.format(properties_str))
+        if not (c1 or c2):
+            properties_str = ', '.join(
+                map(lambda x: '\'{0}\',\'{1}\''.format(x[0], x[1]), d.items()))
+            c1 = self.execute('g.addV({})'.format(properties_str))
+            assert 'name' in d, d
+            logger.debug('add_node: Added node with properties {}'.format(d))
+        else:
+            if c1:
+                logger.debug(
+                    'add_node: Node with name {} already exists'.format(n))
+            if c2:
+                logger.debug(
+                    'add_node: Node with dictionary {} already exists'.format(d))
+        return c1
+
+    def load_from_document_graph(self, dg):
+        for n1, n2 in dg.g.edges():
+            d1 = dg.g.node[n1]
+            d2 = dg.g.node[n2]
+            d = dg.g.edge[n1][n2]
+            label = d['type']
+            self.add_edge(n1, d1,
+                n2, d2, d, label)
+
+    def read_into_document_graph(self):
+        doc = Document()
+        node_id2name_map = {}
+        nodes = self.all_nodes()
+        if not nodes:
+            return DocumentGraph(doc)
+        for v in nodes:
+            d = v['properties']
+            assert 'type' in d, d
+            assert 'name' in d, d
+            resource_id = d['name'][0]['value']
+            node_id2name_map[v['id']] = resource_id
+            resource_type = d['type'][0]['value']
+            att_val_list = [
+                (str(k), str(val[0]['value']))
+                for (k, val) in d.items()
+                if k not in ['name', 'type']]
+            r = ResourceFactory.create(
+                resource_type, resource_id, att_val_list)
+            doc.expression_list.append(r)
+        edges = self.all_edges()
+        for e in edges:
+            d = e['properties']
+            assert 'type' in d
+            event_type = d['type']
+            event_timestamp = d['timestamp'] if 'timestamp' in d else None
+            att_val_list = [
+                (k, val)
+                for (k, val) in d.items()
+                if k not in ['timestamp', 'type']]
+            ev = EventFactory.create(
+                event_type,
+                node_id2name_map[e['outV']],
+                node_id2name_map[e['inV']],
+                att_val_list, event_timestamp)
+            doc.expression_list.append(ev)
+        return DocumentGraph(doc)
+
+    def drop_db(self):
+        r = self.execute('g.V().drop().iterate()')
+        assert r == None
+
+
+def test():
+    sys.stderr.write('*' * 30 + '\n')
+    sys.stderr.write('Running PROVN Segmenter Tests\n')
+    sys.stderr.write('*' * 30 + '\n')
+    provn_file = 'test/test_james.provn'
+    broker = 'ws://localhost:8182/'
+    doc = Document()
+    sys.stderr.write('---> Parsing {0}\n'.format(provn_file))
+    doc.parse_provn(provn_file)
+    in_dg = DocumentGraph(doc)
+    tc = TitanClient(broker)
+    tc.drop_db()
+    sys.stderr.write('---> Loading {0} into DB\n'.format(provn_file))
+    tc.load_from_document_graph(in_dg)
+    sys.stderr.write('---> Reading {0} from DB\n'.format(provn_file))
+    out_dg = tc.read_into_document_graph()
+    tc.close()
+
+    print('LOADED INTO DB:')
+    print(in_dg.doc)
+    print('READ FROM DB:')
+    print(out_dg.doc)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='A simple wrapper around a Titan DB')
+    parser.add_argument('provn_file', help='A prov-tc file in provn format')
+    parser.add_argument('broker', help='The broker to the Titan DB')
+    parser.add_argument('--verbose', '-v', action='store_true',
+        help='Run in verbose mode')
+
+    args = parser.parse_args()
+    VERBOSE = args.verbose
+    # Check that provided non-optional files actually exist
+    for f in [args.provn_file]:
+        if not (os.path.isfile(f)):
+            print('File {0} does not exist...aborting'.format(f))
+
+    assert not args.broker or re.match('.+:\d+', args.broker), 'Broker must be in format url:port'
+
+    doc = Document()
+    doc.parse_provn(args.provn_file)
+    in_dg = DocumentGraph(doc)
+    tc = TitanClient(args.broker)
+    tc.drop_db()
+    tc.load_from_document_graph(in_dg)
+    out_dg = tc.read_into_document_graph()
+    tc.close()
+
+    print('LOADED INTO DB:')
+    print(in_dg.doc)
+    print('READ FROM DB:')
+    print(out_dg.doc)
