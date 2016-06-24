27c006e43434c68ed387b4496b8100c7e2e04a24
Thomas M. DuBuisson <tommd@galois.com>
2016-06-24 16:42:03 -0700

    Handle common exception, send info to dashboard.

--- fake-fe.py
+++ fake-fe.py
@@ -7,19 +7,34 @@ import kafka
 from kafka.common import ConsumerTimeout
 
 from os import getenv
+import sys
 
 def main():
     ta3Host = '127.0.0.1'
     toPX = 'pe'
     toSE = 'se'
+    toLog = 'pe-log'
 
     kafkaServer = ta3Host + ':9092'
+    print("Connecting to Kafka...", file=sys.stderr)
     producer = kafka.KafkaProducer(bootstrap_servers=[kafkaServer])
+    print("Connected producer to Kafka", file=sys.stderr)
     consumer = kafka.KafkaConsumer(toPX, bootstrap_servers=[kafkaServer])
+    print("Connected consumer to Kafka", file=sys.stderr)
     print(consumer.subscription())
     for msg in consumer:
+      print("Got a message: %s" % msg.value, file=sys.stderr)
       if msg.value == b'\x01':
         producer.send(toSE, b'\x01')
+        producer.send(toLog, b'Signaled for segmenter to start.')
+
+def safe():
+    try:
+        main()
+    except kafka.errors.NoBrokersAvailable:
+        print("Kafka failed, No brokers available", file=sys.stderr)
 
 if __name__ == '__main__':
-    main()
+    print("Feature Extractor has started.",file=sys.stderr)
+    while True:
+        safe()
