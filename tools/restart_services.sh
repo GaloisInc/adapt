8fead94065989f27ab4000c201e7dd6420d36c5d
Thomas M. DuBuisson <tommd@galois.com>
2016-06-24 15:14:09 -0700

    Add a --clean option to restart_services.sh

--- tools/restart_services.sh
+++ tools/restart_services.sh
@@ -3,5 +3,8 @@
 # Sends a signal to supervisord that indicates it should re-read the
 # configuration file and kill or restart daemons as necessary given the delta.
 /opt/titan/bin/titan.sh stop
+if [ "$1" = "--clean" ]; then
+	/opt/titan/bin/titan.sh clean
+fi
 /opt/titan/bin/titan.sh start
 killall -HUP supervisord
