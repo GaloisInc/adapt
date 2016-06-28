91d24ef58a87bec8fa7901f2df9edfd949fb87c1
Erin Chapman <erin@galois.com>
2016-06-28 11:38:26 -0700

    sync adapt master and adapt-segment master branches

--- segmenter/titandb.py
+++ segmenter/titandb.py
@@ -7,17 +7,20 @@
 
     Adria Gascon, 2016.
 """
-import asyncio
 from aiogremlin import GremlinClient
 from provn_segmenter import DocumentGraph, Document
 from provnparser import ResourceFactory, EventFactory
-import re
 import argparse
-import os
-import sys
+import asyncio
 import logging
+import os
+import pprint
+import re
+import sys
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 
 def escape(s):
     """
@@ -25,7 +28,7 @@ def escape(s):
     Replaces qutes with their escaped versions.  Other escaping
     may be necessary to avoid problems.
     """
-    return s.replace("\'","\\\'").replace("\"","\\\"")
+    return s.replace("\'", "\\\'").replace("\"", "\\\"")
 
 
 class TitanClient:
@@ -38,21 +41,22 @@ class TitanClient:
         self.loop.run_until_complete(self.gc.close())
         self.loop.close()
 
+
     def execute(self, gremlin_query_str, bindings={}):
-        execute = self.gc.execute(gremlin_query_str, bindings=bindings)
-        logger.debug('QUERY:\n {}'.format(gremlin_query_str))
-        try:
-            result = self.loop.run_until_complete(execute)
-        except Exception as e:
-            print('Error trying to connect to Titan DB: {0}...aborting'.
-                format(e))
-            self.close()
-            sys.exit(-1)
-        assert result[0].status_code in (200, 204, 206), result[0].status_code
-        if result == 204:
-            return None
-        else:
-            return result[0].data
+        @asyncio.coroutine
+        def stream(gc):
+            result_data = []
+            resp = yield from gc.submit(gremlin_query_str, bindings=bindings)
+            while True:
+                result = yield from resp.stream.read()
+                if result is None:
+                    break
+                assert result.status_code in [206, 200, 204], result.status_code
+                if not result.status_code == 204:
+                    result_data += result.data
+            return result_data
+        result = self.loop.run_until_complete(stream(self.gc))
+        return result
 
     def all_edges(self):
         return self.execute('g.E()')
@@ -71,12 +75,13 @@ class TitanClient:
         id1 = r1[0]['id']
         id2 = r2[0]['id']
         properties_str = ', '.join(
-            map(lambda x: '\'{0}\',\'{1}\''.format(x[0], x[1]) if x[0] != 'label' else "", d.items()))
+            map(lambda x: '\'{0}\',\'{1}\''.format(x[0], x[1])
+                if x[0] != 'label' else "", d.items()))
         r = self.execute(
             'g.V({0}).next().addEdge(\'{2}\', g.V({1}).next(), {3})'.format(
                 id1, id2, label, properties_str))
         return r
-    
+
     def add_node(self, n, d):
         """
         Adds node with name n to the DB if it does not already exist.
@@ -92,9 +97,11 @@ class TitanClient:
         c2 = self.execute('g.V().and({0})'.format(properties_str))
         if not (c1 or c2):
             properties_str = ', '.join(
-                map(lambda x: '\'{0}\',\'{1}\''.format(x[0], escape(x[1])), d.items()))
+                map(lambda x: '\'{0}\',\'{1}\''.format(
+                    x[0], escape(x[1])), d.items()))
             # Hack: for now, label all new nodes as segment nodes.
-            c1 = self.execute('g.addV(label,\'Segment\',{})'.format(properties_str))
+            c1 = self.execute(
+                'g.addV(label,\'Segment\',{})'.format(properties_str))
             assert 'ident' in d, d
             logger.debug('add_node: Added node with properties {}'.format(d))
         else:
@@ -103,7 +110,7 @@ class TitanClient:
                     'add_node: Node with name {} already exists'.format(n))
             if c2:
                 logger.debug(
-                    'add_node: Node with dictionary {} already exists'.format(d))
+                    'add_node: Node with dict {} already exists'.format(d))
         return c1
 
     def load_from_document_graph(self, dg):
@@ -113,8 +120,7 @@ class TitanClient:
             d = dg.g.edge[n1][n2]
             label = d['label']
             self.add_edge(n1, d1,
-                n2, d2, d, label)
-
+                          n2, d2, d, label)
 
     def load_segments_from_document_graph(self, dg):
         """
@@ -128,8 +134,8 @@ class TitanClient:
             label = d['label']
             if label.startswith('segment:'):
                 self.add_edge(n1, d1,
-                    n2, d2, d, label)
-            
+                              n2, d2, d, label)
+
     def read_into_document_graph(self):
         doc = Document()
         node_id2name_map = {}
@@ -197,7 +203,7 @@ if __name__ == "__main__":
     parser.add_argument('provn_file', help='A prov-tc file in provn format')
     parser.add_argument('broker', help='The broker to the Titan DB')
     parser.add_argument('--verbose', '-v', action='store_true',
-        help='Run in verbose mode')
+                        help='Run in verbose mode')
 
     args = parser.parse_args()
     VERBOSE = args.verbose
