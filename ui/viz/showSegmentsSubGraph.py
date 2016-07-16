4a60e578274e03d9f8e9a43497f676150035ab37
Rui Maranhao <rui.maranhao@gmail.com>
2016-07-15 22:54:16 -0700

    clean strange characters

--- ui/viz/showSegmentsSubGraph.py
+++ ui/viz/showSegmentsSubGraph.py
@@ -1,33 +1,32 @@
 #! /usr/bin/env python3
-​
+
 import os
 import sys
 import pprint
 import asyncio
 from aiogremlin import GremlinClient
 import graphviz
-​
+
 sys.path.append(os.path.expanduser('~/adapt/tools'))
 import cdm.enums
 import gremlin_properties
 import gremlin_query
-​
+
 QUERYV = "g.V().hasLabel('Segment')"
 QUERYE = "g.V({}).as('a').out('segment:includes').out().in('segment:includes').where(neq('a'))"
-​
+
 def toDot(graph, label='Segmentation Graph'):
     dot = graphviz.Digraph(graph_attr={'label': label,
                                        'labelloc': 't',
                                        'fontname': 'sans-serif'},
                            node_attr={'margin': '0',
                                       'fontsize': '6',
-​
                                       'fontname': 'sans-serif'})
     for n in graph.keys():
         linecolor, color, penwidth = ('black', 'white', '1')
         fontcolor = linecolor
         node_label = graph[n]['name'] + ':' + str(graph[n]['criteria'])
-​
+
         dot.node(str(n),
                  node_label,
                  style='filled',
@@ -35,16 +34,16 @@ def toDot(graph, label='Segmentation Graph'):
                  color=linecolor,
                  fontcolor=fontcolor,
                  penwidth=penwidth)
-​
+
         for o in graph[n]['edges_out']:
             dot.edge(str(n), str(o), color=linecolor)
-​
+
     return dot
-​
+
 if __name__ == '__main__':
-​
+
     with gremlin_query.Runner() as gremlin:
-​
+
         vertices = gremlin.fetch(QUERYV)[0].data
         if vertices:
             graph = {}
