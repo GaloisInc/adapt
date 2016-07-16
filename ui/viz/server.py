e1e164872aa81a79e09c2601f3ad4db892bef97c
Rui Maranhao <rui.maranhao@gmail.com>
2016-07-16 12:51:07 -0700

    final touches in flask server

--- ui/viz/server.py
+++ ui/viz/server.py
@@ -1,12 +1,11 @@
 import os
 
-from flask import Flask
-app = Flask(__name__, static_url_path='')
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
 
 @app.route('/')
 def service():
     os.system("./genSegGraph.sh")
     return render_template('index.html')
-
 
 app.run(host='0.0.0.0', port=8180)
