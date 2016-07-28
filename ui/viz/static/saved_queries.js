64cf17ad9675667b76ed87fdf7e742ff9ad1bf50
Ryan Wright <rrwright@gmail.com>
2016-07-27 22:49:24 -0400

    saved collapsed queries via right click (on only some nodes)

---
+++ ui/viz/static/saved_queries.js
@@ -0,0 +1,13 @@
+var saved_queries = [
+    {
+        // This query is just an example. It should be deleted once a few more have been created.
+        "name" : "2 Hops Away (limit 10)",
+        "is_relevant" : function(n) {return n.label === "Subject"},
+        "floating_query" : ".bothE().bothV().bothE().bothV().limit(10)"
+    },
+    {
+        "name" : "Previous File Version",
+        "is_relevant" : function(n) {return n.label === "Entity-File" && n['properties']['file-version'][0]['value'] > 1},
+        "floating_query" : ".outE('EDGE_OBJECT_PREV_VERSION out').inV().outE('EDGE_OBJECT_PREV_VERSION in').inV()"
+    }
+]
