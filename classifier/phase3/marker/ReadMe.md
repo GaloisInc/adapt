b60439a17f41f89945ce611c6925ca4bcf97afa6
John Hanley <John.Hanley@parc.com>
2016-08-04 16:00:08 -0700

    Add instrumented build script.

--- classifier/phase3/marker/ReadMe.md
+++ classifier/phase3/marker/ReadMe.md
@@ -9,9 +9,9 @@ injecting begin / end markers for grammar terminals into
 the sequence of logged events:
 
 1. preamble events
-2. marker: begin A 
+2. marker: begin 1
 3. events of interest
-4. marker: end A 
+4. marker: end 1
 5. postamble events
 
 Our sklearn.ensemble.RandomForestClassifier model will
@@ -22,3 +22,8 @@ During training we deliberately blind the learner
 to attributes easily controlled by an attacker,
 such as names of files outside of `/usr/bin`,
 substituting a nonce value instead.
+
+For marker number 1, the begin event shows up in
+the trace as a write to `/tmp/marker-001-begin.txt`.
+This is followed by events of interest,
+and then a write to `/tmp/marker-001-end.txt`.
---
+++ classifier/phase3/marker/build_instrumented_simple_apt.sh
@@ -0,0 +1,43 @@
+#! /usr/bin/env bash
+
+# Copyright 2016, Palo Alto Research Center.
+# Developed with sponsorship of DARPA.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# The software is provided "AS IS", without warranty of any kind, express or
+# implied, including but not limited to the warranties of merchantability,
+# fitness for a particular purpose and noninfringement. In no event shall the
+# authors or copyright holders be liable for any claim, damages or other
+# liability, whether in an action of contract, tort or otherwise, arising from,
+# out of or in connection with the software or the use or other dealings in
+# the software.
+#
+
+#
+# usage:
+#    ./build_instrumented_simple_apt.sh
+#
+
+DEST=/tmp/kudu/build
+mkdir -p $DEST
+cd $DEST
+test -d TransparentComputing ||
+    git clone git@github.com:kududyn/TransparentComputing.git
+set -x
+cd TransparentComputing/malware/cross-platform/simple-apt/simple/
+(cd src && etags *.c linux/*.c)
+git checkout -- src/tcp.c
+git status
+patch -p5 < ~/adapt/classifier/phase3/marker/tcp_patch.txt
+which cmake || sudo apt-get install -y cmake gcc-multilib g++-multilib
+python cmake.py build
+rsync -av bin/linux/release/x64/simple /tmp/
