3d1a695a95c478a02b2370395a4c51cdf0b736a4
James Cheney <jcheney@inf.ed.ac.uk>
2016-08-09 12:14:49 +0000

    adapt_segmenter renamed in test

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -70,7 +70,7 @@ def execute_module(module_key, data_key="5d_youtube_ie_output-100.avro"):
         os.system('Trint -f')
         sleep(1)  # currently no way to know when other services are complete with their respective processing.
     elif module_key is "se":
-        cmd = ('%s/segment/segmenter/adapt_DBsideSegmenter.py'
+        cmd = ('%s/segment/segmenter/adapt_Segmenter.py'
                ' --broker http://localhost:8182/'
                ' --store-segment Yes --spec %s/config/segmentByPID.json' % (top, top))
         os.system(cmd)
