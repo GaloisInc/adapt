9b72be8ca720e2ff767dfdfa56a4e97f8d9256bd
James Cheney <jcheney@inf.ed.ac.uk>
2016-08-11 18:44:43 +0100

    time-based progressive segmenting, using indexing but (for now) without segment:edge edges

--- segment/segmenter/bytime.groovy
+++ segment/segmenter/bytime.groovy
@@ -8,39 +8,37 @@ def endHour(t) { endWindow(t,1000*1000*60*60) }
 def startMin(t) { startWindow(t,1000*1000*60) }
 def endMin(t) { endWindow(t,1000*1000*60) }
 
-def getTimes(g) { g.V().has('startedAtTime').values('startedAtTime').is(gt(0)) }
+def getTimes(g) { g.V().has('startedAtTime').values('startedAtTime') }
 
-def addTimeSegments(graph,g,delta) {
-  segments =  g.V().has('startedAtTime')
-               .values('startedAtTime')
-               .is(gt(0))
-               .map{t = it.get(); t - t % delta}.dedup().order();
-  vtime = 0;
-  v = graph.addVertex(label,'Segment',
-                      'segment:name','byTime',
-                      'startedAtTime',0);
-  content = g.V().has('startedAtTime').has(label,neq('Segment')).as('a').values('startedAtTime').is(gte(vtime).and(lt(vtime+delta))).select('a');
-  for(z in content) {
-    v.addEdge('segment:includes',z) 
-  }    
-  for(s in segments) {
-    w = graph.addVertex(label,'Segment','segment:name','byTime','startedAtTime',s);
-    v.addEdge('segment:edge',w);
+def addTimeSegmentsFromTo(graph,g,delta) {
  
-    v = w;
-    vtime = s
-    content = g.V().has('startedAtTime').has(label,neq('Segment')).as('a').values('startedAtTime').is(gte(vtime).and(lt(vtime+delta))).select('a');
+  segments =  g.V().has('startedAtTime',gte(0)).values('startedAtTime').map{t = it.get(); t - t % delta}.dedup().order();
+  for(s in segments) {
+    v = graph.addVertex(label,'Segment','segment:name','byTime','startedAtTime',s,'endedAtTime',s+delta);
+    content = g.V().has('startedAtTime',gte(s).and(lt(s+delta))).has(label,neq('Segment'));
     for(z in content) {
       v.addEdge('segment:includes',z) 
     } 
   }
-  
+}
+
+def addTimeSegmentsFromTo(graph,g,Integer delta,Long start,Long end} =
+  start = start - start % delta;
+  end = end - end % delta + delta;
+  segments =  g.V().has('startedAtTime',gte(start).and(lt(end))).values('startedAtTime').map{t = it.get(); t - t % delta}.dedup().order();
+  for(s in segments) {
+    v = graph.addVertex(label,'Segment','segment:name','byTime','startedAtTime',s,'endedAtTime',s+delta);
+    content = g.V().has('startedAtTime',gte(s).and(lt(s+delta))).has(label,neq('Segment'));
+    for(z in content) {
+      v.addEdge('segment:includes',z) 
+    } 
+  }
 }
 
 
 
 def removeTimeSegments(g) {
-  g.V().has(label,'Segment').has('segment:name','byTime').drop().iterate()
+  g.V().has('segment:name','byTime').has(label,'Segment').drop().iterate()
 }
 
 addTimeSegments(graph,g,1000*1000*60)
