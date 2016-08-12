7a17322848cd3636c68ec90ce124cb1fbdd94a8d
James Cheney <jcheney@inf.ed.ac.uk>
2016-08-12 14:37:43 +0100

    added indexing experiment scripts and moved scripts to subdirectory

--- segment/segmenter/groovy/bypid-ts.groovy
+++ segment/segmenter/groovy/bypid-ts.groovy
@@ -1,11 +1,11 @@
 
 
 def removePIDSegments(g) {
-  g.V().has('segment:name','byPID').has(label,'Segment').drop().iterate()
+  g.V().has(label,'Segment').drop().iterate()
 }
 
 def getPIDSegments(g) {
-  g.V().has('segment:name','byPID').has(label,'Segment')
+  g.V().has(label,'Segment')
 }
 
 // creates segment vertices for PID nodes, excluding already-existing ones
@@ -15,7 +15,7 @@ def getPIDSegments(g) {
 def createVertices(graph,g,criterion) {
   t1 = new Date().getTime();
   idWithProp = g.V().has(criterion,gte(0)).has(label,neq('Segment')).id().fold().next();
-  existingSegNodes_parentIds = g.V().has('segment:name','byPID').values('parentVertexId').fold().next();
+  existingSegNodes_parentIds = g.V().hasLabel('Segment').values('parentVertexId').fold().next();
   idsToStore = idWithProp-existingSegNodes_parentIds; 
   if (idsToStore!=[]) { 
     for (i in idsToStore) {
@@ -37,7 +37,7 @@ def segmentNodesCreated(g) {
 def createVerticesAndEdges(graph,g,criterion,radius) {
   t1 = new Date().getTime();
   idWithProp = g.V().has(criterion,gte(0)).has(label,neq('Segment')).id().fold().next(); 
-  existingSegNodes_parentIds = g.V().has('segment:name','byPID').values('parentVertexId').fold().next();
+  existingSegNodes_parentIds = g.V().hasLabel('Segment').values('parentVertexId').fold().next();
   idsToStore = idWithProp - existingSegNodes_parentIds; 
   for (i in idWithProp) {
     sub = g.V(i).repeat(__.bothE().subgraph('sub').bothV().has(label,neq('Segment'))).times(radius).cap('sub').next();
@@ -45,10 +45,10 @@ def createVerticesAndEdges(graph,g,criterion,radius) {
     if (i in idsToStore) { 
       s = graph.addVertex(label,'Segment','segment:name','byPID','pid',g.V(i).values('pid').next(),'parentVertexId',i)
     } else {
-      s = g.V().has('segment:name','byPID').has('parentVertexId',i).next()
+      s = g.V().hasLabel('Segment').has('parentVertexId',i).next()
     }; 
     idNonLinkedNodes = subtr.V().id().fold().next()
-                     - g.V().has('segment:name','byPID').has('parentVertexId',i).outE('segment:includes').inV().id().fold().next();
+                     - g.V().hasLabel('Segment').has('parentVertexId',i).outE('segment:includes').inV().id().fold().next();
     for (node in idNonLinkedNodes) {
       s.addEdge('segment:includes',g.V(node).next())
     }
@@ -60,11 +60,16 @@ def createVerticesAndEdges(graph,g,criterion,radius) {
 
 
 def createSeg2SegEdges(graph,g) {
-  for (snode in g.V().has('segment:name','byPID').id().fold().next()) {
+  t1 = new Date().getTime();
+  for (snode in g.V().hasLabel('Segment').id().fold().next()) {
     linkedSeg = g.V(snode).as('a').out('segment:includes').out().in('segment:includes').dedup().where(neq('a')).id().fold().next()
               - g.V(snode).out('segment:edge').id().fold().next();
     for (s in linkedSeg) { 
       g.V(snode).next().addEdge('segment:edge',g.V(s).next())
     }
   }
+  t2 = new Date().getTime();
+  return t2 - t1;
 }
+
+//ts  = new Long[5]; for (int i = 0; i < 5; i++) { createVerticesAndEdges(graph,g,'pid',2); ts[i] = createSeg2SegEdges(graph,g); removePIDSegments(g) } 
