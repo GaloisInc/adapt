6eea9570e2cca41b26a786cd377f9765d07ba9c2
berradag <berrada.ghita@gmail.com>
2016-08-17 12:27:23 +0100

    added composite index for 'parentVertexId' and 'emit()' step in subgraph construction (to fetch nodes within radius r and not just nodes at radius r)

--- segment/segmenter/groovy/bypid-noindex.groovy
+++ segment/segmenter/groovy/bypid-noindex.groovy
@@ -40,15 +40,14 @@ def createVerticesAndEdges(graph,g,criterion,radius) {
   existingSegNodes_parentIds = g.V().hasLabel('Segment').values('parentVertexId').fold().next();
   idsToStore = idWithProp - existingSegNodes_parentIds; 
   for (i in idWithProp) {
-    sub = g.V(i).repeat(__.bothE().subgraph('sub').bothV().has(label,neq('Segment'))).times(radius).cap('sub').next();
+    sub = g.V(i).emit().repeat(__.bothE().subgraph('sub').bothV().has(label,neq('Segment'))).times(radius).cap('sub').next();
     subtr = sub.traversal(); 
     if (i in idsToStore) { 
       s = graph.addVertex(label,'Segment','segment:name','byPID','pid',g.V(i).values('pid').next(),'parentVertexId',i)
     } else {
       s = g.V().hasLabel('Segment').has('parentVertexId',i).next()
     }; 
-    idNonLinkedNodes = subtr.V().id().fold().next()
-                     - g.V().hasLabel('Segment').has('parentVertexId',i).outE('segment:includes').inV().id().fold().next();
+    idNonLinkedNodes = subtr.V().id().fold().next()- g.V().hasLabel('Segment').has('parentVertexId',i).outE('segment:includes').inV().id().fold().next();
     for (node in idNonLinkedNodes) {
       s.addEdge('segment:includes',g.V(node).next())
     }
@@ -62,8 +61,7 @@ def createVerticesAndEdges(graph,g,criterion,radius) {
 def createSeg2SegEdges(graph,g) {
   t1 = new Date().getTime();
   for (snode in g.V().hasLabel('Segment').id().fold().next()) {
-    linkedSeg = g.V(snode).as('a').out('segment:includes').out().in('segment:includes').dedup().where(neq('a')).id().fold().next()
-              - g.V(snode).out('segment:edge').id().fold().next();
+    linkedSeg = g.V(snode).as('a').out('segment:includes').out().in('segment:includes').dedup().where(neq('a')).id().fold().next()- g.V(snode).out('segment:edge').id().fold().next();
     for (s in linkedSeg) { 
       g.V(snode).next().addEdge('segment:edge',g.V(s).next())
     }
