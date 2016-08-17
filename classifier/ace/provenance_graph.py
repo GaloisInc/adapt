972e620240166c38c6087c507feee3ce0f71252b
Alexander Feldman <alex@llama.gs>
2016-08-16 19:06:11 -0700

    Implemented an unsupervised activity classifier so we can have some ground truth labels for the supervised learning methods.

---
+++ classifier/ace/provenance_graph.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+import networkx
+import pprint
+
+from ace.titan_database import TitanDatabase
+
+class ProvenanceGraph(object):
+    def __init__(self):
+        self.titanClient = TitanDatabase()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+
+    def close(self):
+        self.titanClient.close()
+
+    def createActivity(self, segmentId, name):
+        # @todo: figure out transactions or do it in one query
+        nodeQuery = "graph.addVertex(label, 'Activity', 'activity:type', '{}')".format(name)
+        node = self.titanClient.execute(nodeQuery)
+        activityId = node[0]['id']
+        edgeQuery = "g.V({}).next().addEdge('{}', g.V({}).next())".format(segmentId, 'activity:includes', activityId)
+        edge = self.titanClient.execute(edgeQuery)
+
+        return node, edge
+
+    def deleteActivities(self):
+        query = "g.V().has(label, 'Activity').drop().iterate()"
+        # Removing a vertex removes all its incident edges as well.
+        result = self.titanClient.execute(query)
+
+    def activityNodes(self):
+        query = "g.V().has(label, 'Activity')"
+        nodes = self.titanClient.execute(query)
+        for node in nodes:
+            yield node
+
+    def segmentNodes(self):
+        query = "g.V().has(label, 'Segment')"
+        nodes = self.titanClient.execute(query)
+        for node in nodes:
+            yield node
+
+    def segments(self):
+        query = "g.V().has(label, 'Segment')"
+        nodes = self.titanClient.execute(query)
+        for node in nodes:
+            G = networkx.Graph()
+
+            nodeId = node['id']
+            G.add_node(nodeId)
+
+            query = "g.V({}).as('a').out('segment:includes').out().in('segment:includes').where(neq('a'))"
+            adjacentNodes = self.titanClient.execute(query.format(nodeId))
+            for adjacentNode in adjacentNodes:
+                adjacentNodeId = adjacentNode['id']
+                G.add_node(adjacentNodeId)
+                G.add_edge(nodeId, adjacentNodeId)
+
+            yield(nodeId, G)
---
+++ classifier/ace/titan_database.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+"""Low-level Titan database client.
+"""
+
+from aiogremlin import GremlinClient
+import asyncio
+import aiohttp
+
+class TitanDatabase(object):
+    def __init__(self, broker = 'ws://localhost:8182/'):
+        self.broker = broker
+        self.loop = asyncio.get_event_loop()
+        self.gc = GremlinClient(url = broker, loop = self.loop)
+
+    def close(self):
+        self.loop.run_until_complete(self.gc.close())
+        self.loop.close()
+
+    def execute(self, gremlin_query_str, bindings={}):
+        @asyncio.coroutine
+        def stream(gc):
+            result_data = []
+            response = yield from gc.submit(gremlin_query_str, bindings=bindings)
+            while True:
+                result = yield from response.stream.read()
+                if result is None:
+                    break
+                assert(result.status_code in [206, 200, 204], result.status_code)
+                if not result.status_code == 204:
+                    result_data += result.data
+            return result_data
+
+        result = self.loop.run_until_complete(stream(self.gc))
+
+        return result
