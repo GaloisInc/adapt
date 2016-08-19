19b8a73f4ef92be86f02daeca2ffd7533fb8c60a
Alexander Feldman <alex@llama.gs>
2016-08-19 12:39:05 -0700

    Implemented the supervised activity classifier.

--- classifier/create_classifier.py
+++ classifier/create_classifier.py
@@ -2,9 +2,7 @@
 
 import os
 import sys
-import numpy
 
-from ace.titan_database import TitanDatabase
 from ace.provenance_graph import ProvenanceGraph
 from ace.supervised_classifier import SupervisedClassifier
 from ace.feature_extractor import FeatureExtractor
@@ -13,6 +11,4 @@ if __name__ == '__main__':
     with ProvenanceGraph() as provenanceGraph:
         featureExtractor = FeatureExtractor()
         activityClassifier = SupervisedClassifier(provenanceGraph, featureExtractor)
-        labels, X = activityClassifier.createTrainingData()
-
-        numpy.savez('training_data.npz', labels, X)
+        activityClassifier.createClassifier('classifier.pkl')
