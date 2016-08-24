61b581a402ae913725f6fde4576ed87a50e9b731
James Cheney <jcheney@inf.ed.ac.uk>
2016-08-24 16:44:34 +0100

    tabbing

--- pylib/titanDB.py
+++ pylib/titanDB.py
@@ -18,37 +18,37 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class TitanClient:
-        def __init__(self, broker='ws://localhost:8182/'):
-                self.broker = broker
-                self.loop = asyncio.get_event_loop()
-                self.gc = GremlinClient(url=broker, loop=self.loop)
+	def __init__(self, broker='ws://localhost:8182/'):
+		self.broker = broker
+		self.loop = asyncio.get_event_loop()
+		self.gc = GremlinClient(url=broker, loop=self.loop)
 
-        def close(self):
-                self.loop.run_until_complete(self.gc.close())
-                self.loop.close()
+	def close(self):
+		self.loop.run_until_complete(self.gc.close())
+		self.loop.close()
 
-        def execute(self, gremlin_query_str, bindings={}):
-                @asyncio.coroutine
-                def stream(gc):
-                        result_data = []
-                        try:
-                                resp = yield from gc.submit(gremlin_query_str, bindings=bindings)
-                        except aiohttp.errors.ClientOSError as e:
-                                sys.stdout.write("Cannot connect to "+self.broker+"\n")
-                                sys.exit()
-                        else:
-                                while True:
-                                        result = yield from resp.stream.read()
-                                        if result is None:
-                                                break
-                                        assert result.status_code in [206, 200, 204], result.status_code
-                                        if not result.status_code == 204:
-                                                result_data += result.data
-                        return result_data
-                result = self.loop.run_until_complete(stream(self.gc))
-                return result
+	def execute(self, gremlin_query_str, bindings={}):
+		@asyncio.coroutine
+		def stream(gc):
+			result_data = []
+			try:
+				resp = yield from gc.submit(gremlin_query_str, bindings=bindings)
+			except aiohttp.errors.ClientOSError as e:
+				sys.stdout.write("Cannot connect to "+self.broker+"\n")
+				sys.exit()
+			else:
+				while True:
+					result = yield from resp.stream.read()
+					if result is None:
+						break
+					assert result.status_code in [206, 200, 204], result.status_code
+					if not result.status_code == 204:
+						result_data += result.data
+			return result_data
+		result = self.loop.run_until_complete(stream(self.gc))
+		return result
 
-		def fetch_many(self,queries, sem_num):
+	def fetch_many(self,queries, sem_num):
 			sem=asyncio.Semaphore(sem_num)
 			@asyncio.coroutine
 			def fetch(name,query): 
@@ -60,12 +60,12 @@ class TitanClient:
 					print("Finished: %s in %fs" % (name,t2-t1))
 					return (name,query,result)
 				
-            jobs = [fetch(name,query) for (name,query) in queries]
+	    jobs = [fetch(name,query) for (name,query) in queries]
 			results = self.loop.run_until_complete(asyncio.gather(*jobs))
 			return results   
 
 
-        def drop_db(self):
-                r = self.execute('g.V().drop().iterate()')
-                count=self.execute('g.V().count()')[0]
-                assert count is 0
+	def drop_db(self):
+		r = self.execute('g.V().drop().iterate()')
+		count=self.execute('g.V().count()')[0]
+		assert count is 0
