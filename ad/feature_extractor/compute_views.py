eaef6eedbefc09a0717073488b93a363b5e558e6
Md Amran Siddiqui <siddiqmd@oregonstate.edu>
2016-08-27 23:01:22 -0700

    Added anomalyFeature property for easy explanation

--- ad/feature_extractor/compute_views.py
+++ ad/feature_extractor/compute_views.py
@@ -146,21 +146,31 @@ class AnomalyView:
         cutoff = math.ceil(total_nodes * (percentage / 100.0))
         max_score = 0
         max_id = 0
+        max_feature = None
         cnt = 0
         QUERY = ""
-        binds = {'atype':'anomalyType', 'ascore':'anomalyScore', 'sin':'segment:includes'}
+        binds = {'atype':'anomalyType', 'ascore':'anomalyScore', 'sin':'segment:includes', 'afeature':'anomalyFeature'}
         with open(self.score_file, 'r') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
                 QUERY += "x={id};t='{type}';s={score};g.V(x).property(atype,t).next();g.V(x).property(ascore,s).next();".format(id=row['id'], type=self.view_type, score=row['anomaly_score'])
+                feature = "["
+                for k in sorted(row.keys()):
+                    if k != 'id' and k != 'anomaly_score':
+                        if feature != "[":
+                            feature += ","
+                        feature += k + ":" + str(row[k])
+                feature += "]"
+                QUERY += "f='{feat}';g.V(x).property(afeature,f).next();".format(feat=feature)
                 log.info("Adding anomaly scores to id " + row['id'] + " (" + self.view_type + ", " + row['anomaly_score'] + ")")
                 if float(row['anomaly_score']) > max_score:
                     max_score = float(row['anomaly_score'])
                     max_id = row['id']
+                    max_feature = feature
                 cnt = cnt + 1
                 if cnt >= cutoff:
                     break
-            QUERY += "x={id};t='{type}';s={score};IDS=g.V(x).in(sin).id().toList().toArray();if(IDS!=[]){{g.V(IDS).property(atype,t).next();g.V(IDS).property(ascore,s).next()}};".format(id=max_id, type=self.view_type, score=max_score)
+            QUERY += "x={id};t='{type}';s={score};f='{feat}';IDS=g.V(x).in(sin).id().toList().toArray();if(IDS!=[]){{g.V(IDS).property(atype,t).next();g.V(IDS).property(ascore,s).next();g.V(IDS).property(afeature,f).next();}};".format(id=max_id, type=self.view_type, score=max_score, feat=max_feature)
             log.info("size of QUERY = " + str(len(QUERY)))
             log.info("Attaching anomaly scores to top " + str(cutoff) + " anomalous nodes (threshold=" + str(percentage) + "%)...")
             try:
