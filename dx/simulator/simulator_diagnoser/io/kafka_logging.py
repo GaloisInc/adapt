332b0ae8eb924b2b2792f0549a010f08c222d81c
Alexandre Perez <alexandrecperez@gmail.com>
2016-09-14 13:07:43 -0700

    Wait between KafkaProducer retries.

--- dx/simulator/simulator_diagnoser/io/kafka_logging.py
+++ dx/simulator/simulator_diagnoser/io/kafka_logging.py
@@ -6,9 +6,14 @@ class KafkaHandler(logging.Handler):
     def __init__(self, url='localhost:9092', topic='dx-log'):
         logging.Handler.__init__(self)
         self.topic = topic
-        self.producer = kafka.KafkaProducer(bootstrap_servers=[url])
+        while self.producer is None:
+            try:
+                self.producer = kafka.KafkaProducer(bootstrap_servers=[url], retries=10)
+            except kafka.errors.NoBrokersAvailable:
+                time.sleep(2)
 
     def emit(self, record):
-        entry = self.format(record)
-        self.producer.send(self.topic, entry.encode())
-        self.producer.flush()
+        if self.producer:
+            entry = self.format(record)
+            self.producer.send(self.topic, entry.encode())
+            self.producer.flush()
