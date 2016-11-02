464a3daad5d3a8787be70cd00dc9148ad0799ebc
Erin Chapman <erin@galois.com>
2016-11-02 14:34:33 -0700

    Add CDM support (initial)

--- Ingest/IngestDaemon/Ingest.java
+++ Ingest/IngestDaemon/Ingest.java
@@ -15,42 +15,124 @@ import java.io.IOException;
 import org.apache.avro.file.DataFileReader;
 import org.apache.avro.io.DatumReader;
 import org.apache.avro.specific.SpecificDatumReader;
+import com.google.common.collect.Lists;
+import java.util.List;
 
 public class Ingest {
     public static void main(String[] args) throws IOException {
-        /*TitanGraph graph = TitanFactory.open("/opt/titan/conf/gremlin-server/titan-cassandra-server.properties");
+        System.out.println(readAvroFile("test.avro"));
+
+        System.exit(0);
+    }
+
+    private static List<TCCDMDatum> readAvroFile(String file) throws IOException {
+        List<TCCDMDatum> results = Lists.newArrayList();
+        File f = new File(file);
+        DatumReader<TCCDMDatum> tcDatumReader = new SpecificDatumReader<TCCDMDatum>(TCCDMDatum.class);
+        DataFileReader<TCCDMDatum> tcFileReader = new DataFileReader<TCCDMDatum>(f, tcDatumReader);
+        TCCDMDatum tcd = null;
+        while(tcFileReader.hasNext()) {
+            tcd = tcFileReader.next();
+            results.add(tcd);
+        }
+
+        return results;
+    }
+
+    private static void insertIntoTitan(List<TCCDMDatum> data) {
+        TitanGraph graph = TitanFactory.open("/opt/titan/conf/gremlin-server/titan-cassandra-server.properties");
+
+        try {
+            AbstractObject o = (AbstractObject) data;
+        }
+        catch (Exception e)
+        {
+        }
+        try {
+            FileObject o = (FileObject) data;
+        }
+        catch (Exception e)
+        {
+        }
+        try {
+            MemoryObject o = (MemoryObject) data;
+        }
+        catch (Exception e)
+        {
+        }
+        try {
+            NetFlowObject o = (NetFlowObject) data;
+        }
+        catch (Exception e)
+        {
+        }
+        try {
+            Principal o = (Principal) data;
+        }
+        catch (Exception e)
+        {
+        }
+        try {
+            SrcSinkObject o = (SrcSinkObject) data;
+        }
+        catch (Exception e)
+        {
+        }
+        try {
+            Subject o = (Subject) data;
+        }
+        catch (Exception e)
+        {
+        }
 
         // see org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerFactory.generateClassic()
-        final Vertex f1 = graph.addVertex("Entity-File");
-        f1.property("ident", 1);
-        final Vertex f2 = graph.addVertex("Entity-File");
-        f2.property("ident", 2);
-        final Vertex f3 = graph.addVertex("Entity-File");
-        f3.property("ident", 3);
-        final Vertex f4 = graph.addVertex("Entity-File");
-        f4.property("ident", 4);
-        f1.addEdge("EDGE_EVENT_AFFECTS_FILE out", f2);
-        f2.addEdge("EDGE_EVENT_AFFECTS_FILE out", f3);
-        f3.addEdge("EDGE_EVENT_AFFECTS_FILE out", f4);
+        //f1.addEdge("EDGE_EVENT_AFFECTS_FILE out", f2);
 
-        GraphTraversalSource g = graph.traversal();
+/*        GraphTraversalSource g = graph.traversal();
         Vertex fromNode = g.V().has("ident", 1).next();
         Vertex toNode = g.V().has("ident", 4).next();
         ArrayList list = new ArrayList();
         g.V(fromNode).repeat(both().simplePath()).until(is(toNode)).limit(1).path().fill(list);
         System.out.println("\n\n\nFOUND PATH:");
         System.out.println(list);
-        System.out.println("\n\n\n");
-*/
-        File file = new File("test.avro");
-        DatumReader<TCCDMDatum> tcDatumReader = new SpecificDatumReader<TCCDMDatum>(TCCDMDatum.class);
-        DataFileReader<TCCDMDatum> tcFileReader = new DataFileReader<TCCDMDatum>(file, tcDatumReader);
-        TCCDMDatum tcd = null;
-        while(tcFileReader.hasNext()) {
-            tcd = tcFileReader.next();
-            System.out.println(tcd);
-        }
-
-        System.exit(0);
+        System.out.println("\n\n\n");*/
     }
+
+    private static void insertEntity(TitanGraph g, TCCDMDatum d) {
+        final Vertex v = g.addVertex("Entity");
+        Object o = d.getDatum();
+        v.property("ident", 1);
+    }
+
+    /*
+       AbstractObject -> Entity
+       ConfidentialityTag ->
+       EdgeType ->
+       Event ->
+       EventType ->
+       FileObject -> Entity-File
+       InstrumentationSource ->
+       IntegrityTag ->
+       MemoryObject -> Entity-Memory
+       NetFlowObject -> Entity-NetFlow
+       Principal -> Agent
+       PrincipalType ->
+       ProvenanceTagNode ->
+       RegistryKeyObject ->
+       SHORT ->
+       SimpleEdge ->
+       SrcSinkObject -> Resource
+       SrcSinkType ->
+       Subject -> Subject
+       SubjectType ->
+       TagEntity ->
+       TagOpCode ->
+       TCCDMDatum ->
+       UUID ->
+       ValueDataType ->
+       Value ->
+       ValueType ->
+
+Unmapped: Host
+    */
 }
