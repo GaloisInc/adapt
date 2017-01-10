f04eb2a623578290afc9951018ab3fd7031c711d
Alec Theriault <alec.theriault@gmail.com>
2017-01-10 14:28:15 -0800

    Check hash for adapt.jar in adapt-tester
    Add a check that the hash of the 'adapt.jar' downloaded from adapt-tester matches one on
    adapt.galois.com. This extra hash is now also automatically created and uploaded in the build
    script.

--- AdaptJVM/deploy_test_tool.sh
+++ AdaptJVM/deploy_test_tool.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
+assembledAdapt="./target/scala-2.11/adapt-assembly-0.1.jar"
+assembledScepter="./scepter/target/scala-2.11/scepter-assembly-0.1.jar"
+
+#remote="adapt.galois.com~/acceptance_tests"
+remote="alfred:/srv/www/adapt.galois.com/public_html/acceptance_tests"
+
 rm ./adapt-tester.jar 2> /dev/null
 rm ./adapt.jar 2> /dev/null
 sbt clean
-sbt assembly && scp target/scala-2.11/adapt-assembly-0.1.jar adapt.galois.com:~/acceptance_tests/adapt.jar
-sbt scepter/assembly && scp scepter/target/scala-2.11/scepter-assembly-0.1.jar adapt.galois.com:~/acceptance_tests/adapt-tester.jar
 
+# Upload the actual adapt jar
+sbt assembly && \
+scp $assembledAdapt ${remote}/adapt.jar
+
+# Upload the hash for the adapt jar (queried by adapt-tester)
+md5 -q $assembledAdapt > ./adapt.hash && \
+scp ./adapt.hash ${remote}/current.hash
+rm ./adapt.hash 2> /dev/null
+
+# Upload the adapt-tester jar
+sbt scepter/assembly && \
+scp $assembledScepter ${remote}/adapt-tester.jar
+
+# Upload the hash for the adapt-tester jar
+md5 -q $assembledScepter > ./scepter.hash && \
+scp ./scepter.hash ${remote}/current-tester.hash
+rm ./scepter.hash 2> /dev/null
+
+# Download/run the latest adapt-tester
 wget http://adapt.galois.com/adapt-tester.jar && \
-java -jar ./adapt-tester.jar ./Engagement1DataCDM13/pandex/ta1-clearscope-cdm13_pandex.bin && \
-rm ./adapt-tester.jar
+java -jar ./adapt-tester.jar ./Engagement1DataCDM13/pandex/ta1-clearscope-cdm13_pandex.bin
+rm ./adapt-tester.jar 2> /dev/null
+
--- AdaptJVM/scepter/src/main/scala/com/galois/adapt/scepter/Wrapper.scala
+++ AdaptJVM/scepter/src/main/scala/com/galois/adapt/scepter/Wrapper.scala
@@ -1,7 +1,6 @@
 package com.galois.adapt.scepter
 
 import java.io._
-import java.math.BigInteger
 import java.net.URL
 import java.nio.channels.Channels
 import java.nio.file.{Files, Paths}
@@ -22,30 +21,29 @@ object Wrapper extends App {
   )
 
   val r = Try {
-    print("Getting latest tests...")
+    // Download the jar
+    print("Getting latest tests... ")
     val in = Channels.newChannel(new URL(downloadUrl).openStream)
     val out = new FileOutputStream(executableJarPath).getChannel
     out.transferFrom(in, 0, Long.MaxValue)
-    print(" ...done.\n")
+    println("done.")
+
+    // Calculate the MD5 hash
+    val fileHash = s"md5 -q $executableJarPath".!!.trim
+    
+    // Fetch expected hash
+    val hashUrl = new URL("https://adapt.galois.com/acceptance_tests/current.hash")
+    val statedHash = new Scanner(hashUrl.openStream()).useDelimiter("\\A").next().trim
+    
+    // The hashes should match equal
+    require(
+      fileHash.equals(statedHash), 
+      s"""Hash comparison failed. The download file is corrupt or there was an error on the server
+         |Stated hash:   $statedHash
+         |Computed hash: $fileHash
+         |""".stripMargin
+    )
   }
-
-  val fileHash = Try {
-    val md = MessageDigest.getInstance("MD5")
-    val is = Files.newInputStream(Paths.get(executableJarPath))
-    val dis = new DigestInputStream(is, md)
-    val bytes = md.digest()
-    val bi = new BigInteger(1, bytes)
-    val digest = String.format("%0" + (bytes.length << 1) + "X", bi)
-    digest.toLowerCase
-  }
-//  println(s"Hash of downloaded file:\n${fileHash.get}")
-
-
-  val hashUrl = new URL("https://adapt.galois.com/acceptance_tests/current.hash")
-  val statedHash = new Scanner(hashUrl.openStream()).useDelimiter("\\A").next()
-//  println(s"Stated hash:\n$statedHash")
-
-//  require(fileHash.get.equals(statedHash), s"Hash comparison failed. The download file is corrupt or there was an error on the server\nStated hash:   $statedHash\nComputed hash: ${fileHash.get}\n")
 
   val cmd = s"java -Xmx4G -Dadapt.app=accept -Dadapt.loadlimit=0 -Dadapt.loadfile=$dataFilePath -jar $executableJarPath"
 
@@ -57,6 +55,6 @@ object Wrapper extends App {
       println(s"Running tests on the data. This could take a moment...")
       println(cmd.!!)
     case f: Failure[_] =>
-      println(s"Getting new tests failed: ${f.exception.getMessage}")
+      println(s"Getting new tests failed:\n ${f.exception.getMessage}")
   }
 }
