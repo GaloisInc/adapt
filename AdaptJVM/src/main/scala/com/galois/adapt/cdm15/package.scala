54873760125431475c2beeed32dc62e287a00aff
Alec Theriault <alec.theriault@gmail.com>
2017-03-01 09:51:24 -0800

    Make type error clearer with annotations

--- AdaptJVM/src/main/scala/com/galois/adapt/cdm15/package.scala
+++ AdaptJVM/src/main/scala/com/galois/adapt/cdm15/package.scala
@@ -22,20 +22,20 @@ package object cdm15 {
     val values = Seq(Principal, ProvenanceTagNode, TagRunLengthTuple, Value, CryptographicHash, Subject, AbstractObject, FileObject, UnnamedPipeObject, RegistryKeyObject, NetFlowObject, MemoryObject, SrcSinkObject, Event, UnitDependency, TimeMarker, TCCDMDatum)
 
     def readData(filePath: String, limit: Option[Int] = None): Try[Iterator[Try[CDM15]]] = readAvroFile(filePath).map { x =>
-      val cdmDataIter = x.map(CDM15.parse)
+      val cdmDataIter: Iterator[Try[CDM15]] = x.map(CDM15.parse)
       limit.fold(cdmDataIter)(l => cdmDataIter.take(l))
     }
 
-    def readAvroFile(filePath: String) = Try {
+    def readAvroFile(filePath: String): Try[Iterator[RawCDM15Type]] = Try {
       val tcDatumReader = new SpecificDatumReader(classOf[com.bbn.tc.schema.avro.cdm15.TCCDMDatum])
       val tcFileReader: DataFileReader[com.bbn.tc.schema.avro.cdm15.TCCDMDatum] = new DataFileReader(new java.io.File(filePath), tcDatumReader)
       val tcIterator = tcFileReader.iterator.asScala
 
       val cdm = tcIterator.next()
-      val first = {
+      val first: RawCDM15Type = {
         new TCCDMDatum(cdm.getSource)
       }
-      val second = {
+      val second: RawCDM15Type = {
         if (cdm.CDMVersion.toString != "15")
           throw new Exception(s"Expected CDM15, but received CDM${cdm.CDMVersion.toString}")
         new RawCDM15Type(cdm.getDatum)
