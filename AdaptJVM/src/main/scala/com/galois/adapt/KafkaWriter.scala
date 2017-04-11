4f7465c7ff4f8f04f4be58d3ed048433f81ca5ee
Erin Chapman <erin@galois.com>
2017-04-11 11:58:14 -0700

    WIP on kafka ingest

--- AdaptJVM/src/main/scala/com/galois/adapt/KafkaWriter.scala
+++ AdaptJVM/src/main/scala/com/galois/adapt/KafkaWriter.scala
@@ -1,12 +1,18 @@
 package com.galois.adapt
 
 import akka.actor._
-
+import com.bbn.tc.schema.avro.cdm17.TCCDMDatum
+import org.apache.avro.file.DataFileReader
+import org.apache.avro.specific.SpecificDatumReader
+import org.apache.avro.util.Utf8
 
 class KafkaWriter(val files: List[String]) extends Actor with ActorLogging {
   files.foreach { readAvroFile }
 
-  def readAvroFile(file: String): Unit {}
+  def readAvroFile(file: String): Unit = {
+    val tcDatumReader = new SpecificDatumReader(classOf[com.bbn.tc.schema.avro.cdm17.TCCDMDatum])
+    val tcFileReader: DataFileReader[com.bbn.tc.schema.avro.cdm17.TCCDMDatum] = new DataFileReader(new java.io.File(file), tcDatumReader)
+  }
 
   def receive = {
     case (file: String) =>
