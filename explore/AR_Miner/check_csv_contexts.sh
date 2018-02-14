bba736fac5f7d01a02a177e2da0a1b0390b9f609
SB <sidahmed63@gmail.com>
2018-02-14 14:40:41 +0000

    csv comparison

--- explore/AR_Miner/check_csv_contexts.sh
+++ explore/AR_Miner/check_csv_contexts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 echo "Now Running The CSV Files Comparison"
 a='./contexts/ProcessEventSample.csv'
-b='./contexts/ProcessEventSample2.csv'
+b='./contexts/ProcessEventSample_altered.csv'
 Rscript compare_csv.r $a $b 
  
 
