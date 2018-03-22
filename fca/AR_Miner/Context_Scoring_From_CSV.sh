dd439c22acfe8c7d1735f9dee4cdea4513ae0763
SB <sidahmed63@gmail.com>
2018-03-22 12:17:09 +0000

    change sup conf settinggs

--- fca/AR_Miner/Context_Scoring_From_CSV.sh
+++ fca/AR_Miner/Context_Scoring_From_CSV.sh
@@ -3,6 +3,6 @@ context_name='ProcessEvent'
 csv_file='./contexts/Context_ProcessEvent.csv'
 rcf_file='./contexts/Context_ProcessEvent.rcf'
 output_scoring_file='./contexts/Scoring_Of_Context_ProcessEvent.csv'
-MinSup='90'
-MinConf='90'
+MinSup='97'
+MinConf='97'
 Rscript contexts_scoring_shell.r $context_name $csv_file $rcf_file $output_scoring_file $MinSup $MinConf
