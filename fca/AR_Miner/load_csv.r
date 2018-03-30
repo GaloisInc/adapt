a4cd3a26849a31ed67c3f75866dbe40b432347ea
SB <sidahmed63@gmail.com>
2018-03-30 18:02:14 +0100

    assert csv file existence

--- fca/AR_Miner/load_csv.r
+++ fca/AR_Miner/load_csv.r
@@ -1,9 +1,13 @@
 #### this function takes as input a csv context and loads a data structure
-load_csv=function(csv_file_name){
+load_csv=function(csv_file){
   #csv_file_name="/home/terminator2/Documents/R_Projects/AnomalyRulesMining/contexts/Context_ProcessEvent.csv"
   Incon = file(csv_file, "r")
   if(is.null(Incon))stop("error in csv_file_name     - terminating")
- # context_file=read.csv(file='./contexts/Context_ProcessEvent.csv')
+  if(!file.exists(csv_file))stop(" csv file does not exist")
+  
+  if (file.size(csv_file) == 0)  {stop("\n csv_file is file empty \n")}
+  
+  # context_file=read.csv(file='./contexts/Context_ProcessEvent.csv')
   cat('loading csv')
   context_file=NULL
   result = tryCatch({
@@ -21,6 +25,11 @@ load_csv=function(csv_file_name){
   context_matrix <- as.matrix(context_file)
   ObjectList=as.list(context_matrix[,1])
   context_matrix<- context_matrix[,-1]
+  
+  if(length(ObjectList)==0)stop("\n Length of List_Objects is 0, try other configurations or databases \n")
+  if(length(AttribuesList)==0)stop("\n Length of List_Attributes is 0, try other configurations or databases \n")
+  
+  
   cat('loading ObjectOfAttributes')
   
   for (i in 1:length(AttribuesList)){
