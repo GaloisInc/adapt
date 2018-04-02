82802b6113bfdb9f95c6f87d53eb164d75bca5b5
Ryan Wright <rrwright@gmail.com>
2018-04-02 11:59:33 -0700

    minor settings change

--- bbn-deploy.sh
+++ bbn-deploy.sh
@@ -6,7 +6,7 @@ STAMP=${STAMP//:/-}
 STAMP="packaged_on-$STAMP"
 touch ./$STAMP
 echo "Packaging into: adapt.tar.gz"
-tar zcf ./adapt.tar.gz --exclude=".git/" --exclude="fca/" --exclude="target/" --exclude="neo4j.db" --exclude=".idea/" --exclude="adapt.tar.gz" --exclude=".DS_Store" ./
+tar zcf ./adapt.tar.gz --exclude=".git/" --exclude="iforest.exe" --exclude="legacy/" --exclude="fca/" --exclude="target/" --exclude="neo4j.db" --exclude=".idea/" --exclude="adapt.tar.gz" --exclude=".DS_Store" ./
 scp ./adapt.tar.gz bbn:~/
 rm ./$STAMP
 rm ./adapt.tar.gz
