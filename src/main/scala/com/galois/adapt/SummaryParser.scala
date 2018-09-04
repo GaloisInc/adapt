0c9b23d9ca1b7cfb4f8850f0a26343aad37e28fe
Aditya <aditya.zutshi@galois.com>
2018-09-04 15:01:39 -0700

    readableSummary works but relies on multiple traversals.

--- src/main/scala/com/galois/adapt/SummaryParser.scala
+++ src/main/scala/com/galois/adapt/SummaryParser.scala
@@ -1,9 +1,9 @@
+// what is difference b/w process forks and execute
 package com.galois.adapt
 
 import java.util.UUID
 
-import com.galois.adapt.cdm18.{EVENT_ACCEPT, EVENT_CLOSE, EVENT_EXIT, EVENT_FORK, EVENT_LSEEK, EVENT_MMAP, EVENT_OPEN, EVENT_OTHER, EVENT_READ, EVENT_RECVFROM, EVENT_WRITE, EventType, FileObjectType, SrcSinkType}
-import org.neo4j.cypher.internal.frontend.v2_3.ast.Collection
+import com.galois.adapt.cdm18.{EVENT_ACCEPT, EVENT_CLOSE, EVENT_EXECUTE, EVENT_EXIT, EVENT_FORK, EVENT_LSEEK, EVENT_MMAP, EVENT_MODIFY_PROCESS, EVENT_OPEN, EVENT_OTHER, EVENT_READ, EVENT_RECVFROM, EVENT_WRITE, EventType, FileObjectType, SrcSinkType}
 
 import scala.reflect.ClassTag
 
@@ -16,12 +16,23 @@ object SummaryParser {
   def countActivities[A<:Element](l:List[Element], f:A=>Boolean = (_:A)=>true)(implicit tag: ClassTag[A]): Int = l.count{case a:A if f(a)=> true; case _ => false}
   def filter[A<:Element](l:List[Element], f:A=>Boolean = (_:A)=>true)(implicit tag: ClassTag[A]): List[A] = l.collect{case a:A if f(a)=> a}
 
+  def isFileActivity(a:ProcessActivity):Boolean = a match {case a:ProcessFileActivity => true; case _ => false}
+  def isNWActivity(a:ProcessActivity):Boolean = a match {case a:ProcessNWActivity => true; case _ => false}
+  def isProcessActivity(a:ProcessActivity):Boolean = a match {case a:ProcessProcessActivity => true; case _ => false}
+  def isSrcSinkActivity(a:ProcessActivity):Boolean = a match {case a:ProcessSrcSinkActivity => true; case _ => false}
+
+  //def isOfType[A](a:ProcessActivity):Boolean = {case _:A => true; case _ => false}
+
   // elementary activities
-  def isNWRead(a:ProcessNWActivity) = a.event == EVENT_RECVFROM
-  def isNWWrite(a:ProcessNWActivity) = a.event == EVENT_WRITE
-  def isFileWrite(a:ProcessFileActivity) = a.event == EVENT_WRITE
-  def isFileRead(a:ProcessFileActivity) = a.event == EVENT_READ || a.event == EVENT_MMAP
-  def isProcessFork(a:ProcessProcessActivity) = a.event == EVENT_FORK
+  def isNWRead(a:ProcessNWActivity): Boolean = a.event == EVENT_RECVFROM
+  def isNWWrite(a:ProcessNWActivity): Boolean = a.event == EVENT_WRITE
+  def isFileWrite(a:ProcessFileActivity): Boolean = a.event == EVENT_WRITE
+  def isFileRead(a:ProcessFileActivity): Boolean = a.event == EVENT_READ || a.event == EVENT_MMAP
+  def isFileExec(a:ProcessFileActivity): Boolean = a.event == EVENT_EXECUTE
+  def isProcessFork(a:ProcessProcessActivity): Boolean = a.event == EVENT_FORK
+  def isProcessModify(a:ProcessProcessActivity): Boolean = a.event == EVENT_MODIFY_PROCESS
+
+  def areTwoFilesSame(a1:ProcessFileActivity,a2:ProcessFileActivity): Boolean = a1.filePath == a2.filePath
 
   //def countFileReads(l:List[Element]): Int = {countActivities[ProcessFileActivity](l, _.event==EVENT_READ)}
 //  def countFileWrites(l:List[Element]): Int = {countActivities[ProcessFileActivity](l, _.event==EVENT_WRITE)}
@@ -31,9 +42,16 @@ object SummaryParser {
 //  def countUniqueUuids(l:List[ProcessActivity]): Int = {l.map(_.subject.uuid).toSet.size}
   def countFileReads(l:List[Element]): Int = countActivities(l, isFileRead)
   def countFileWrites(l:List[Element]): Int = countActivities(l, isFileWrite)
+  def countFileExecs(l:List[Element]): Int = countActivities(l, isFileExec)
   def countFileActivities(l:List[Element]): Int = {countActivities[ProcessFileActivity](l)}
+
   def countNWActivities(l:List[Element]): Int = {countActivities[ProcessNWActivity](l)}
+  def countNWReads(l:List[Element]): Int = countActivities(l, isNWRead)
+  def countNWWrites(l:List[Element]): Int = countActivities(l, isNWWrite)
+
   def countProcessActivities(l:List[Element]): Int = {countActivities[ProcessActivity](l)}
+  def countProcessForks(l:List[Element]): Int = {countActivities(l, isProcessFork)}
+  def countProcessModifications(l:List[Element]): Int = {countActivities(l, isProcessModify)}
 
   def countUniqueUuids(l:List[ProcessActivity]): Int = {l.map(_.subject.uuid).toSet.size}
 
@@ -41,18 +59,25 @@ object SummaryParser {
   //Sub-Activities
   def getFilesReadActivities(l:List[Element]): List[ProcessFileActivity] = filter(l, isFileRead)
   def getFilesWrittenActivities(l:List[Element]): List[ProcessFileActivity] = filter(l, isFileWrite)
+  def getFilesExecActivities(l:List[Element]): List[ProcessFileActivity] = filter(l, isFileExec)
   def getNWReadsActivities(l:List[Element]): List[ProcessNWActivity] = filter(l, isNWRead)
   def getNWWritesActivities(l:List[Element]): List[ProcessNWActivity] = filter(l, isNWWrite)
   def getProcessForkActivities(l:List[Element]): List[ProcessProcessActivity] = filter(l, isProcessFork)
+  def getProcessModifyActivities(l:List[Element]): List[ProcessProcessActivity] = filter(l, isProcessModify)
 
   // Unique predicate objects
   def getUuids(l:List[Element]): Set[UUID] = getProcessActivities(l).map(_.subject.uuid).toSet
 
   def getFilesRead(l:List[Element]): Set[FilePath] = getFilesReadActivities(l).map(_.filePath).toSet
   def getFilesWritten(l:List[Element]): Set[FilePath] = getFilesWrittenActivities(l).map(_.filePath).toSet
+  def getFilesExecuted(l:List[Element]): Set[FilePath] = getFilesExecActivities(l).map(_.filePath).toSet
   def getNWReads(l:List[Element]): Set[NWEndpointRemote] = getNWReadsActivities(l).map(_.neRemote).toSet
   def getNWWrites(l:List[Element]): Set[NWEndpointRemote] = getNWWritesActivities(l).map(_.neRemote).toSet
   def getProcessForks(l:List[Element]): Set[ProcessPath] = getProcessForkActivities(l).map(_.subject2.processPath).toSet
+  def getProcessModified(l:List[Element]): Set[ProcessPath] = getProcessModifyActivities(l).map(_.subject2.processPath).toSet
+
+
+
 
   /*
   def FRA(f:FilePath) = (i:ProcessFileActivity) => i.filePath == f && i.event == EVENT_READ
@@ -96,22 +121,46 @@ object SummaryParser {
 
     case Nil => {if(acc.size%2 == 0) acc else acc.tail}.reverse
   }
-  def collectConsecutive[A1<:Element,A2<:Element](paList:List[ProcessActivity], f1:A1 => Boolean, f2:A2 => Boolean, track:Boolean=false, acc:List[ProcessActivity]=List.empty)(implicit tag1: ClassTag[A1], tag2: ClassTag[A2]):List[ProcessActivity] = paList match {
-    case (head:A1)::tail if !track && f1(head) => {collectConsecutive(tail,f1,f2, true, head::acc)}
-    case (head:A1)::tail if track && f1(head) => {collectConsecutive(tail,f1,f2, true, head::acc.tail)}
+  def collectConsecutive__[A1<:ProcessActivity,A2<:ProcessActivity](paList:List[ProcessActivity], f1:A1 => Boolean, f2:A2 => Boolean, track:Boolean=false, acc:List[ProcessActivity]=List.empty)(implicit tag1: ClassTag[A1], tag2: ClassTag[A2]):List[ProcessActivity] = {
+    //implicit f1, f2, f12
+    paList match {
+      case (head: A1) :: tail if !track && f1(head) => collectConsecutive__(tail, f1, f2, true, head :: acc)
+      case (head: A1) :: tail if track && f1(head) => collectConsecutive__(tail, f1, f2, true, head :: acc.tail)
 
-    case (head:A2)::tail if track && f2(head) => collectConsecutive(tail,f1,f2, false, head::acc)
+      case (head: A2) :: tail if track && f2(head) => collectConsecutive__(tail, f1, f2, false, head :: acc)
 
-    case _::tail if track => collectConsecutive(tail,f1,f2, false, acc.tail)
-    case _::tail if !track => collectConsecutive(tail,f1,f2,false, acc)
+      case _ :: tail if track => collectConsecutive__(tail, f1, f2,  false, acc.tail)
+      case _ :: tail if !track => collectConsecutive__(tail, f1, f2,  false, acc)
 
-    case Nil => {if(acc.size%2 == 0) acc else acc.tail}.reverse
+      case Nil => {
+        if (acc.size % 2 == 0) acc else acc.tail
+      }.reverse
+    }
   }
 
+  def collectConsecutive[A1<:ProcessActivity,A2<:ProcessActivity](l:List[ProcessActivity], f1:A1 => Boolean, f2:A2 => Boolean, f12:(A1, A2) => Boolean= (_:A1,_:A2)=>true)(implicit tag1: ClassTag[A1], tag2: ClassTag[A2]):List[(A1,A2)] = {
+    //implicit f1, f2, f12
+    def subFun(paList:List[ProcessActivity], track:Boolean, acc1:List[A1], acc2:List[A2]):List[(A1, A2)] = paList match {
+      case (head: A1) :: tail if !track && f1(head) => subFun(tail, true, head::acc1, acc2)
+      case (head: A1) :: tail if track && f1(head) => subFun(tail, true, head::acc1.tail, acc2)
 
-  def listOfPairs[A](l:List[A]) = l.sliding(2,2).map(x => (x.head, x.tail.head)).toList
+      case (head: A2) :: tail if track && f2(head) && f12(acc1.head, head) => subFun(tail, false, acc1, head::acc2)
 
-//  def getAllActivitiesBetween = ???
+      case _ :: tail if track => subFun(tail, false, acc1.tail, acc2)
+      case _ :: tail if !track => subFun(tail,false, acc1, acc2)
+
+      case Nil => {
+        if (acc1.length == acc2.length) acc1 zip acc2 else acc1.tail zip acc2
+      }.reverse
+    }
+
+    subFun(l, false, List.empty[A1], List.empty[A2])
+  }
+
+  def listOfPairs[A](l:List[A]): List[(A, A)] = l.sliding(2,2).map(x => (x.head, x.tail.head)).toList
+  def formatActPairs(paPair:(ProcessActivity, ProcessActivity)): String =  paPair match {case (a1, a2) => s"\n ${a1.toStr} => ${a2.toStr}"}
+
+  //  def getAllActivitiesBetween = ???
 
   def getDirReads = ???
   def get = ???
@@ -120,26 +169,70 @@ object SummaryParser {
 def readableSummary(l:List[ProcessActivity]): String = {
   if (l.isEmpty) "" else{
     val p = l.head.subject.processPath
+
+    val groupedActivities = l.view.groupBy{
+      case a if isFileActivity(a) => "FileActivity"
+      case a if isNWActivity(a) => "NWActivity"
+      case a if isProcessActivity(a) => "ProcessActivity"
+      case a if isSrcSinkActivity(a) => "SrcSinkActivity"
+    }
+
+    //TODO: Better solution?
+    val allFileActivities = groupedActivities("FileActivity").map{case a:ProcessFileActivity => a}
+    val allNWActivities = groupedActivities("NWActivity").map{case a:ProcessNWActivity=> a}
+    val allProcessActivities = groupedActivities("ProcessActivity").map{case a:ProcessProcessActivity => a}
+    val allSrcSinkActivities = groupedActivities("SrcSinkActivity").map{case a:ProcessSrcSinkActivity => a}
+
+    val groupedFileActivities = allFileActivities.groupBy{
+      case a if isFileRead(a) => "FileRead"
+      case a if isFileWrite(a) => "FileWrite"
+      case a if isFileExec(a) => "FileExec"
+    }
+
+    val groupedNWActivities = allNWActivities.groupBy{
+      case a if isNWRead(a) => "NWRead"
+      case a if isNWWrite(a) => "NWWrite"
+    }
+
+    val groupedProcessActivities = allProcessActivities.groupBy{
+      case a if isProcessFork(a) => "ProcessFork"
+      case a if isProcessModify(a) => "ProcessModify"
+    }
+
+
+
     s"""
        |==================================================
        |======= Process Path: ${p.path}
        |==================================================
-       |Number of File Activities: ${countFileActivities(l)} (Reads: ${countFileReads(l)}, Writes: ${countFileWrites(l)})
-       |Number of NW Activities: ${countNWActivities(l)}
-       |Number of Process Activities: ${countProcessActivities(l)}
-       |Number of UUIDS: ${countUniqueUuids(l)}\n
-       |UUIDS (# of activities): ${getNumActivities(l,getUuids(l), (a:ProcessActivity, f:UUID)=>a.subject.uuid == f).map(prettyPrintSorted)}\n
-       |Files Read (# of activities): ${getNumActivities(l,getFilesRead(l), (a:ProcessFileActivity, f:FilePath)=>a.filePath == f && isFileRead(a)).map(prettyPrintSorted)}\n
-       |Files Written (# of activities): ${getNumActivities(l, getFilesWritten(l), (a: ProcessFileActivity, f: FilePath) => a.filePath == f && isFileWrite(a)).map(prettyPrintSorted)}\n
-       |NW Reads (# of activities): ${getNumActivities(l, getNWReads(l), (a: ProcessNWActivity, nr: NWEndpointRemote) => a.neRemote == nr && isNWRead(a)).map(prettyPrintSorted)}\n
-       |NW Writes (# of activities): ${getNumActivities(l, getNWWrites(l), (a: ProcessNWActivity, nr: NWEndpointRemote) => a.neRemote == nr && isNWWrite(a)).map(prettyPrintSorted)}\n
-       |Process Forked (# of activities): ${getNumActivities(l, getProcessForks(l), (a: ProcessProcessActivity, p: ProcessPath) => a.subject2.processPath == p && isProcessFork(a)).map(prettyPrintSorted)}\n
+       |Number of File Activities: ${countFileActivities(l)} (Reads: ${countFileReads(l)}, Writes: ${countFileWrites(l)}, Execs: ${countFileExecs(l)})
+       |Number of NW Activities: ${countNWActivities(l)}(Reads: ${countNWReads(l)}, Writes: ${countNWWrites(l)})
+       |Number of Process Activities: ${countProcessActivities(l)}(Reads: ${countProcessForks(l)}, Modifications: ${countProcessModifications(l)})
 
-       |File Reads followed by File Writes (downloads?): ${listOfPairs(collectConsecutive(l,isFileRead,isNWWrite)).map { case (a1, a2) => s"\n ${a1.toStr} => ${a2.toStr}"; case _ => "" }}
-       |
+       |Number of UUIDS: ${countUniqueUuids(l)}
+       |UUIDS (# of activities): ${getNumActivities(l,getUuids(l), (a:ProcessActivity, f:UUID)=>a.subject.uuid == f).map(prettyPrintSorted)}
+
+       |Files Read (# of activities): ${getNumActivities(l,getFilesRead(l), (a:ProcessFileActivity, f:FilePath)=>a.filePath == f && isFileRead(a)).map(prettyPrintSorted)}
+
+       |Files Written (# of activities): ${getNumActivities(l, getFilesWritten(l), (a: ProcessFileActivity, f: FilePath) => a.filePath == f && isFileWrite(a)).map(prettyPrintSorted)}
+
+       |Files Executed (# of activities): ${getNumActivities(l, getFilesExecuted(l), (a: ProcessFileActivity, f: FilePath) => a.filePath == f && isFileExec(a)).map(prettyPrintSorted)}
+
+       |NW Reads (# of activities): ${getNumActivities(l, getNWReads(l), (a: ProcessNWActivity, nr: NWEndpointRemote) => a.neRemote == nr && isNWRead(a)).map(prettyPrintSorted)}
+
+       |NW Writes (# of activities): ${getNumActivities(l, getNWWrites(l), (a: ProcessNWActivity, nr: NWEndpointRemote) => a.neRemote == nr && isNWWrite(a)).map(prettyPrintSorted)}
+
+       |Process Forked (# of activities): ${getNumActivities(l, getProcessForks(l), (a: ProcessProcessActivity, p: ProcessPath) => a.subject2.processPath == p && isProcessFork(a)).map(prettyPrintSorted)}
+       |Process Modified(# of activities): ${getNumActivities(l, getProcessModified(l), (a: ProcessProcessActivity, p: ProcessPath) => a.subject2.processPath == p && isProcessModify(a)).map(prettyPrintSorted)}
+
+       |NW Reads followed by File Writes (downloads?): ${collectConsecutive(l, isNWRead, isFileWrite).map(formatActPairs)}
+
+       |File Writes followed by File Executes (malicious?): ${collectConsecutive(l, isFileWrite, isFileExec, areTwoFilesSame).map(formatActPairs)}
     """.stripMargin
   }
 }
+  //NW Reads followed by File Writes (downloads?): ${listOfPairs(collectConsecutive__(l,isNWRead,isFileWrite)).map { case (a1, a2) => s"\n ${a1.toStr} => ${a2.toStr}"; case _ => "" }}
+
 
   def f: (ProcessNWActivity, ProcessNWActivity) => Boolean = ProcessNWReadAndNWWrite.isApplicable
   def isEventAccept(a: ProcessActivity): Boolean = a.event == EVENT_ACCEPT
