6fca519e394632331f81879580dc6a61c07d992e
Nichole Schimanski <nls@galois.com>
2019-05-17 14:17:11 -0700

    Fix bugs in E5 splunk parsing script

--- e5_parse_splunk.py
+++ e5_parse_splunk.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import numpy as np
 from collections import Counter
+from collections import defaultdict
 
 # day_boundaries = {'1': {'min': , 'max': },
 #                   '2': {'min': , 'max': },
@@ -36,20 +37,23 @@ def test(filepath):
             alarms.append(line)
     return lines
 
-def read_splunk_log(filepath):
+def read_splunk_log(filepath,ta1=""):
     alarms = {'raw': [], 'aggregatedAlarm': [], 'prioritizedAlarm': []}
-    with open(filepath, 'r') as f:
+    with open(filepath, 'rb') as f:
         lines = f.readlines()
         # runID = lines[0]
-        for line in lines[5:]:
-            if not line.strip(): #(len(line.strip()) != 0):
+        for line in lines[2:]:
+            line = line.strip()
+            if (len(line) != 0) and not line.startswith(b"runID:"):
                 try:
                     alarm_line = json.loads(line)
-                    alarm_category = alarm_line['metadata']['alarm_category']
+                    alarm_category = alarm_line['metadata']['alarmCategory']
                     alarms[alarm_category].append(alarm_line)
                 except:
-                    print(":( \n {}")
+                    print(line)
                     #return None
+                #    print(":( \n {}...".format(line))
+                #    #return None
     return alarms
 
 def datatime_distribution(alarm_list):
@@ -71,23 +75,27 @@ def partition_by_day(alarm_list):
     return partition
 
 def filter_by_proc_names(alarm_list, bad_proc_names):
-    return [a for a in alarm_list if not any([p in a['alarm']['processName'] for p in bad_proc_names])]
+    return [a for a in alarm_list if not any([p in a['alarm']['processName'] \
+                                              for p in bad_proc_names])]
 
 def get_proc_names(alarm_list):
     return [a['alarm']['processName'] for a in alarm_list]
 
-def proc_names_from_file(filename):
+def proc_names_from_file(filename, old_procs_to_ignore):
     procs = []
-    with open(filename, 'r') as f:
-        for line in procs:
-            line = line.strip()
-            if line:
-                procs.append(line)
-    return procs
+    print("Reading process names from {}".format(filename))
+    if filename != "":
+        with open(filename, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                line = ",".join(line.strip().split(",")[:-1])
+                if line:
+                    procs.append(line)
+    return list(set(procs + old_procs_to_ignore))
 
 if __name__ == "__main__":
 
-    PROCS_TO_IGNORE = ["salt"]
+    PROCS_TO_IGNORE = ["svchost","salt"]
 
     parser = argparse.ArgumentParser(description='E5 splunk alarm parser.')
     parser.add_argument('--ta1',
@@ -112,11 +120,13 @@ if __name__ == "__main__":
     ta1 = args.ta1
     splunk_file_path = args.splunklog
 
-    all_procs_to_ignore = proc_names_from_file(args.badprocfile) + PROCS_TO_IGNORE \
-        if args.badprocfile else PROCS_TO_IGNORE
+    all_procs_to_ignore = proc_names_from_file(args.badprocfile, PROCS_TO_IGNORE)
+
+    for line in all_procs_to_ignore:
+        print(line)
 
 
-    alarms = alarms_from_host(read_splunk_log(splunk_file_path)['prioritizedAlarm'], ta1)
+    alarms = alarms_from_host(read_splunk_log(splunk_file_path)['prioritizedAlarm'],ta1)
     filtered_alarms = filter_by_proc_names(alarms, all_procs_to_ignore)
 
     alarms_by_day = partition_by_day(filtered_alarms)
@@ -131,7 +141,8 @@ if __name__ == "__main__":
 
 
     if args.names:
-        process_counter = Counter([p for p in get_proc_names(ps) for ps in alarms_by_day.values()])
+        process_counter = Counter([p for ps in alarms_by_day.values() \
+                                   for p in get_proc_names(ps)])
         process_counter = sorted(process_counter.items(), key = lambda x: -x[-1])
 
         with open(host_name + "_process_names.txt", 'w') as f:
@@ -143,12 +154,27 @@ if __name__ == "__main__":
     if args.details:
         proc_indent = "\t"
         detail_indent = proc_indent * 2
-        with open(ta1 + "_process_details.txt", 'w') as f:
-            for (day, alarm_list) in alarms_by_day:
-                f.write("May {}, 2019 suspicious processes.\n".format(day + 7))
+        with open(host_name + "_process_details.txt", 'w') as f:
+            for (day, alarm_list) in alarms_by_day.items():
+                f.write("\nMay {}, 2019 suspicious processes.\n".format(day + 7))
+                alarm_list = sorted(list(alarm_list),
+                                    key=lambda a: min(a['alarm']['dataTimestamps']))
+                distinct_alarm_dict = defaultdict(list)
                 for alarm in alarm_list:
-                    f.write(proc_indent + alarm['alarm']['processName'])
+                    distinct_alarm_dict[(alarm['alarm']['processName'],
+                                         alarm['alarm']['pid'])].append(alarm)
+                alarm_list = [max(a, key=lambda x: len(x['alarm']['details'])) \
+                                  for a in distinct_alarm_dict.values()]
+                for alarm in alarm_list:
+                    f.write("{} {} - ".format(proc_indent,
+                                              str(min(alarm['alarm']['dataTimestamps']))))
+
+
                     details = alarm['alarm']['details'].split("\n")
                     for line in details:
                         if not "(none)" in line:
-                            f.write(detail_indent + line)
+                            line = ", ".join([l for l in line.split(", ") \
+                                              if ("registry" not in l and \
+                                                  "appdata" not in l)])
+                            if line:
+                                f.write(detail_indent + line + "\n")
